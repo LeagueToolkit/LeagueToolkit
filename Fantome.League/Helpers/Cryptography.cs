426b489206080a8099cf53953a42acfe84f1a64f
Crauzer <filip.quitko@gmail.com>
2017-05-30 22:36:38 +0200

    Hash Functions

--- Fantome.League/Helpers/Cryptography.cs
+++ Fantome.League/Helpers/Cryptography.cs
@@ -8,5 +8,52 @@ namespace Fantome.League.Helpers.Cryptography
 {
     public static class Cryptography
     {
+        public static UInt32 FNV32Hash(string toHash)
+        {
+            toHash = toHash.ToLower();
+            UInt32 hash = 2166136261;
+            for (int i = 0; i < toHash.Length; i++)
+            {
+                hash = hash ^ toHash[i];
+                hash = hash * 16777619;
+            }
+
+            return hash;
+        }
+
+        public static UInt32 SectionHash(string section, string property)
+        {
+            UInt32 hash = 0;
+            section = section.ToLower();
+            property = property.ToLower();
+            for(int i = 0; i < section.Length; i++)
+            {
+                hash = section[i] + 65599 * hash;
+            }
+            hash = (65599 * hash + 42);
+            for (int i = 0; i < property.Length; i++)
+            {
+                hash = property[i] + 65599 * hash;
+            }
+            return hash;
+        }
+
+        public static UInt32 LeagueHash(string toHash)
+        {
+            uint hash = 0;
+            uint temp = 0;
+            toHash = toHash.ToLower();
+            for (int i = 0; i < toHash.Length; i++)
+            {
+                hash = (hash << 4) + toHash[i];
+                temp = hash & 0xF0000000;
+                if (temp != 0)
+                {
+                    hash = hash ^ (temp >> 24);
+                    hash = hash ^ temp;
+                }
+            }
+            return hash;
+        }
     }
 }
