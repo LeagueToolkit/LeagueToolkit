d3665a65d03e0ff8aa89f89aa5efe13b84030752
Crauzer <filip.quitko@gmail.com>
2017-08-03 02:29:19 +0200

    Converter Documentation + Code Clarity

--- Fantome.League/Converters/MeshGeometryConverter.cs
+++ Fantome.League/Converters/MeshGeometryConverter.cs
@@ -1,13 +1,10 @@
 ﻿using Fantome.Libraries.League.Helpers.Structures;
 using Fantome.Libraries.League.IO.OBJ;
-using Fantome.Libraries.League.IO.SCB;
 using Fantome.Libraries.League.IO.SKN;
 using Fantome.Libraries.League.IO.WGEO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
@@ -17,35 +14,36 @@ namespace Fantome.Libraries.League.Converters
     public class MeshGeometryConverter
     {
         /// <summary>
-        /// Converts the specified OBJ File to a <see cref="MeshGeometry3D"/> without normals
+        /// Converts <paramref name="obj"/> to a <see cref="MeshGeometry3D"/>
         /// </summary>
-        /// <param name="obj">The OBJ File which should get converted</param>
-        /// <returns>The converted OBJ File</returns>
+        /// <param name="obj">The <see cref="OBJFile"/> to convert to a <see cref="MeshGeometry3D"/></param>
+        /// <returns>A <see cref="MeshGeometry3D"/> converted from <paramref name="obj"/></returns>
+        /// <remarks>Normals do not get converted</remarks>
         public static MeshGeometry3D ConvertOBJ(OBJFile obj)
         {
             MeshGeometry3D mesh = new MeshGeometry3D();
 
             Int32Collection indices = new Int32Collection();
-            foreach(OBJFace Face in obj.Faces)
+            foreach (OBJFace face in obj.Faces)
             {
-                indices.Add(Face.VertexIndices[0]);
-                indices.Add(Face.VertexIndices[1]);
-                indices.Add(Face.VertexIndices[2]);
+                indices.Add(face.VertexIndices[0]);
+                indices.Add(face.VertexIndices[1]);
+                indices.Add(face.VertexIndices[2]);
             }
 
             Point3DCollection vertices = new Point3DCollection();
-            foreach(Vector3 Vertex in obj.Vertices)
+            foreach (Vector3 vertex in obj.Vertices)
             {
-                vertices.Add(new Point3D(Vertex.X, Vertex.Y, Vertex.Z));
+                vertices.Add(new Point3D(vertex.X, vertex.Y, vertex.Z));
             }
 
-            PointCollection uv = new PointCollection();
-            foreach(Vector2 UV in obj.UVs)
+            PointCollection uvs = new PointCollection();
+            foreach (Vector2 uv in obj.UVs)
             {
-                uv.Add(new Point(UV.X, UV.Y));
+                uvs.Add(new Point(uv.X, uv.Y));
             }
 
-            mesh.TextureCoordinates = uv;
+            mesh.TextureCoordinates = uvs;
             mesh.TriangleIndices = indices;
             mesh.Positions = vertices;
 
@@ -53,57 +51,58 @@ namespace Fantome.Libraries.League.Converters
         }
 
         /// <summary>
-        /// Converts the specified SKN File to a <see cref="MeshGeometry3D"/>
+        /// Converts <paramref name="skn"/> to a list of <see cref="MeshGeometry3D"/>
         /// </summary>
-        /// <param name="skn">The SKN File which should get converted</param>
-        /// <returns>The list of converted SKN Submeshes in a Tuple(SubmeshName, MeshGeometry3D)</returns>
+        /// <param name="skn">The <see cref="SKNFile"/> which should get converted to a <c>Tuple{string, MeshGeometry3D}(submeshName, submeshData)</c></param>
+        /// <returns>A collection of converted <see cref="SKNSubmesh"/></returns>
+        /// <remarks>Normals do not get converted</remarks>
         public static IEnumerable<Tuple<string, MeshGeometry3D>> ConvertSKN(SKNFile skn)
         {
-            foreach(SKNSubmesh Submesh in skn.Submeshes)
+            foreach (SKNSubmesh submesh in skn.Submeshes)
             {
                 MeshGeometry3D mesh = new MeshGeometry3D();
 
                 Int32Collection indices = new Int32Collection();
-                for(int i = 0; i < Submesh.IndexCount; i++)
+                for (int i = 0; i < submesh.IndexCount; i++)
                 {
-                    indices.Add(skn.Indices[i + (int)Submesh.StartIndex] - (int)Submesh.StartIndex);
+                    indices.Add(skn.Indices[i + (int)submesh.StartIndex] - (int)submesh.StartIndex);
                 }
 
                 Point3DCollection vertices = new Point3DCollection();
                 Vector3DCollection normals = new Vector3DCollection();
-                PointCollection uv = new PointCollection();
-                for(int i = 0; i < Submesh.VertexCount; i++)
+                PointCollection uvs = new PointCollection();
+                for (int i = 0; i < submesh.VertexCount; i++)
                 {
-                    SKNVertex vertex = skn.Vertices[i + (int)Submesh.StartVertex];
+                    SKNVertex vertex = skn.Vertices[i + (int)submesh.StartVertex];
                     vertices.Add(new Point3D(vertex.Position.X, vertex.Position.Y, vertex.Position.Z));
                     normals.Add(new Vector3D(vertex.Normal.X, vertex.Normal.Y, vertex.Normal.Z));
-                    uv.Add(new Point(vertex.UV.X, vertex.UV.Y));
+                    uvs.Add(new Point(vertex.UV.X, vertex.UV.Y));
                 }
 
-                mesh.TextureCoordinates = uv;
+                mesh.TextureCoordinates = uvs;
                 mesh.Positions = vertices;
                 mesh.Normals = normals;
                 mesh.TriangleIndices = indices;
 
-                yield return new Tuple<string, MeshGeometry3D>(Submesh.Name, mesh);
+                yield return new Tuple<string, MeshGeometry3D>(submesh.Name, mesh);
             }
         }
 
         /// <summary>
-        /// Converts the specified WGEO File to a list of <see cref="MeshGeometry3D"/> 
+        /// Converts <paramref name="wgeo"/> to a list of <see cref="MeshGeometry3D"/> 
         /// </summary>
-        /// <param name="skn">The WGEO File which should get converted</param>
-        /// <returns>The list of converted SKN Submeshes in a Tuple(MaterialName, TextureName, MeshGeometry3D)</returns>
+        /// <param name="wgeo">The <see cref="WGEOFile"/> to convert to a <c>Tuple{string, string, MeshGeometry3D}(materialName, textureName, modelData)</c></param>
+        /// <returns>A collection of converted <see cref="WGEOModel"/></returns>
         public static IEnumerable<Tuple<string, string, MeshGeometry3D>> ConvertWGEO(WGEOFile wgeo)
         {
-            foreach(WGEOModel Model in wgeo.Models)
+            foreach (WGEOModel Model in wgeo.Models)
             {
                 MeshGeometry3D mesh = new MeshGeometry3D();
 
                 Int32Collection indices = new Int32Collection(Model.Indices.Cast<int>());
                 Point3DCollection vertices = new Point3DCollection();
                 PointCollection uv = new PointCollection();
-                foreach(WGEOVertex Vertex in Model.Vertices)
+                foreach (WGEOVertex Vertex in Model.Vertices)
                 {
                     vertices.Add(new Point3D(Vertex.Position.X, Vertex.Position.Y, Vertex.Position.Z));
                     uv.Add(new Point(Vertex.UV.X, Vertex.UV.Y));
--- Fantome.League/Converters/WGEOConverter.cs
+++ Fantome.League/Converters/WGEOConverter.cs
@@ -1,37 +1,40 @@
 ﻿using Fantome.Libraries.League.IO.NVR;
 using Fantome.Libraries.League.IO.WGEO;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fantome.Libraries.League.Converters
 {
     public static class WGEOConverter
     {
+        /// <summary>
+        /// Converts <paramref name="nvr"/> to a <see cref="WGEOFile"/>
+        /// </summary>
+        /// <param name="nvr">The <see cref="NVRFile"/> to be used for models</param>
+        /// <param name="bucketTemplate">The <see cref="WGEOBucketGeometry"/> to be used a a template for bucket geometry</param>
+        /// <returns>A <see cref="WGEOFile"/> converted from <paramref name="nvr"/></returns>
         public static WGEOFile ConvertNVR(NVRFile nvr, WGEOBucketGeometry bucketTemplate)
         {
             List<WGEOModel> models = new List<WGEOModel>();
 
-            foreach(NVRMesh mesh in nvr.Meshes)
+            foreach (NVRMesh mesh in nvr.Meshes)
             {
                 List<WGEOVertex> vertices = new List<WGEOVertex>();
                 List<ushort> indices = mesh.IndexedPrimitives[0].Indices.Select(x => (ushort)x).ToList();
 
                 foreach (NVRVertex vertex in mesh.IndexedPrimitives[0].Vertices)
                 {
-                    if(mesh.IndexedPrimitives[0].VertexType == NVRVertexType.NVRVERTEX_4)
+                    if (mesh.IndexedPrimitives[0].VertexType == NVRVertexType.NVRVERTEX_4)
                     {
                         NVRVertex4 vertex4 = vertex as NVRVertex4;
                         vertices.Add(new WGEOVertex(vertex4.Position, vertex4.UV));
                     }
-                    else if(mesh.IndexedPrimitives[0].VertexType == NVRVertexType.NVRVERTEX_8)
+                    else if (mesh.IndexedPrimitives[0].VertexType == NVRVertexType.NVRVERTEX_8)
                     {
                         NVRVertex8 vertex8 = vertex as NVRVertex8;
                         vertices.Add(new WGEOVertex(vertex8.Position, vertex8.UV));
                     }
-                    else if(mesh.IndexedPrimitives[0].VertexType == NVRVertexType.NVRVERTEX_12)
+                    else if (mesh.IndexedPrimitives[0].VertexType == NVRVertexType.NVRVERTEX_12)
                     {
                         NVRVertex12 vertex12 = vertex as NVRVertex12;
                         vertices.Add(new WGEOVertex(vertex12.Position, vertex12.UV));
