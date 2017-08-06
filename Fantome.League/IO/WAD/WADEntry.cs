c847f4ba1257258b012986591d3831483f8b911f
Crauzer <filip.quitko@gmail.com>
2017-08-06 21:07:05 +0200

    WAD Clarity

--- Fantome.League/IO/WAD/WADEntry.cs
+++ Fantome.League/IO/WAD/WADEntry.cs
@@ -1,42 +1,39 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
+﻿using System.IO;
 using System.Text;
 
 namespace Fantome.Libraries.League.IO.WAD
 {
-    [DebuggerDisplay("[ Type: {EntryType}, Duplicated: {IsDuplicated}, {BitConverter.ToString(XXHash)}, {Name} ]")]
     public class WADEntry
     {
         public string Name { get; private set; }
         public byte[] XXHash { get; private set; }
-        private UInt32 Offset { get; set; }
-        public UInt32 CompressedSize { get; private set; }
-        public UInt32 UncompressedSize { get; private set; }
+        public uint CompressedSize { get; private set; }
+        public uint UncompressedSize { get; private set; }
         public EntryType Type { get; private set; }
         public bool IsDuplicated { get; private set; }
-        private byte Unknown1 { get; set; }
-        private byte Unknown2 { get; set; }
+        public byte Unknown1 { get; set; }
+        public byte Unknown2 { get; set; }
         public byte[] SHA256 { get; private set; }
         public byte[] Data { get; private set; }
+        private uint _dataOffset { get; set; }
 
-        public WADEntry(BinaryReader br, byte Major, byte Minor)
+        public WADEntry(BinaryReader br, byte major, byte minor)
         {
             this.XXHash = br.ReadBytes(8);
-            this.Offset = br.ReadUInt32();
+            this._dataOffset = br.ReadUInt32();
             this.CompressedSize = br.ReadUInt32();
             this.UncompressedSize = br.ReadUInt32();
             this.Type = (EntryType)br.ReadByte();
             this.IsDuplicated = br.ReadBoolean();
             this.Unknown1 = br.ReadByte();
             this.Unknown2 = br.ReadByte();
-            if (Major == 2 && Minor == 0)
+            if (major == 2 && minor == 0)
                 this.SHA256 = br.ReadBytes(8);
         }
 
         public void ReadData(BinaryReader br)
         {
-            br.BaseStream.Seek((int)this.Offset, SeekOrigin.Begin);
+            br.BaseStream.Seek(this._dataOffset, SeekOrigin.Begin);
             if (this.Type == EntryType.String)
             {
                 this.Name = Encoding.ASCII.GetString(br.ReadBytes((int)br.ReadUInt32()));
@@ -48,7 +45,7 @@ namespace Fantome.Libraries.League.IO.WAD
         }
     }
 
-    public enum EntryType : Byte
+    public enum EntryType : byte
     {
         Uncompressed,
         Compressed,
