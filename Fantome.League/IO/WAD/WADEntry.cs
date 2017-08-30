bf8ab38e1779c1f78ef7f8140e88cb018a97e228
Crauzer <teamlolsabre@gmail.com>
2017-08-30 23:39:16 +0200

    WAD Asynchronous Data Reading and support for big WAD files (#27)
    * WAD Asynchronous Data Reading and support for big WAD files
    
    * More optimized WADEntry reader + moved extension guesser in utilities
    
    * Some doc on new functions on WAD

--- Fantome.League/IO/WAD/WADEntry.cs
+++ Fantome.League/IO/WAD/WADEntry.cs
@@ -15,24 +15,30 @@ namespace Fantome.Libraries.League.IO.WAD
         /// <summary>
         /// Hash of the <see cref="Name"/> of this <see cref="WADEntry"/>
         /// </summary>
-        public byte[] XXHash { get; private set; }
+        public long XXHash { get; private set; }
+
         /// <summary>
         /// Compressed Size of <see cref="Data"/>
         /// </summary>
         public uint CompressedSize { get; private set; }
+
         /// <summary>
         /// Uncompressed Size of <see cref="Data"/>
         /// </summary>
         public uint UncompressedSize { get; private set; }
+
         /// <summary>
         /// Type of this <see cref="WADEntry"/>
         /// </summary>
         public EntryType Type { get; private set; }
+
         /// <summary>
         /// Whether this <see cref="WADEntry"/> is contained in a <see cref="WADFile"/> more than one time
         /// </summary>
         public bool IsDuplicated { get; private set; }
+
         public ushort Unknown1 { get; set; }
+
         /// <summary>
         /// SHA256 Checksum of <see cref="Data"/>
         /// </summary>
@@ -41,15 +47,11 @@ namespace Fantome.Libraries.League.IO.WAD
         /// Only first 8 bytes of the original 32 byte checksum
         /// </remarks>
         public byte[] SHA256 { get; private set; }
-        /// <summary>
-        /// Uncompressed Data of this <see cref="WADEntry"/>
-        /// </summary>
-        public byte[] Data { get; private set; }
 
         /// <summary>
         /// File to load instead of this <see cref="WADEntry"/>
         /// </summary>
-        /// <remarks>Will be <see cref="null"/> if <see cref="Type"/> isnt <c>EntryType.String</c></remarks>
+        /// <remarks>Will be <see cref="null"/> if <see cref="Type"/> isn't <c>EntryType.String</c></remarks>
         public string FileRedirection { get; private set; }
 
         /// <summary>
@@ -57,15 +59,18 @@ namespace Fantome.Libraries.League.IO.WAD
         /// </summary>
         private uint _dataOffset { get; set; }
 
+        private readonly WADFile _wad;
+
         /// <summary>
         /// Reads a <see cref="WADEntry"/> from a <see cref="BinaryReader"/>
         /// </summary>
         /// <param name="br">The <see cref="BinaryReader"/> to read from</param>
         /// <param name="major">Major version of the <see cref="WADFile"/> which is being read</param>
         /// <param name="minor">Minor version of the <see cref="WADFile"/> which is being read</param>
-        public WADEntry(BinaryReader br, byte major, byte minor)
+        public WADEntry(WADFile wad, BinaryReader br, byte major, byte minor)
         {
-            this.XXHash = br.ReadBytes(8);
+            this._wad = wad;
+            this.XXHash = br.ReadInt64();
             this._dataOffset = br.ReadUInt32();
             this.CompressedSize = br.ReadUInt32();
             this.UncompressedSize = br.ReadUInt32();
@@ -73,126 +78,34 @@ namespace Fantome.Libraries.League.IO.WAD
             this.IsDuplicated = br.ReadBoolean();
             this.Unknown1 = br.ReadUInt16();
             if (major == 2 && minor == 0)
+            {
                 this.SHA256 = br.ReadBytes(8);
+            }
+
+            if (Type == EntryType.FileRedirection)
+            {
+                long currentPosition = br.BaseStream.Position;
+                br.BaseStream.Seek(_dataOffset, SeekOrigin.Begin);
+                this.FileRedirection = Encoding.ASCII.GetString(br.ReadBytes(br.ReadInt32()));
+                br.BaseStream.Seek(currentPosition, SeekOrigin.Begin);
+            }
         }
 
         /// <summary>
-        /// Reads the data of this <see cref="WADEntry"/>
+        /// Returns the raw content from the current <see cref="WADEntry"/>.
         /// </summary>
-        /// <param name="br">The <see cref="BinaryReader"/> to read from</param>
-        /// <remarks>The type of data read depends on <see cref="Type"/></remarks>
-        public void ReadData(BinaryReader br)
+        public byte[] GetContent()
         {
-            br.BaseStream.Seek(this._dataOffset, SeekOrigin.Begin);
-            if (this.Type == EntryType.String)
+            byte[] dataBuffer = new byte[CompressedSize];
+            _wad._stream.Seek(_dataOffset, SeekOrigin.Begin);
+            _wad._stream.Read(dataBuffer, 0, (int)CompressedSize);
+            if (Type == EntryType.Compressed)
             {
-                this.FileRedirection = Encoding.ASCII.GetString(br.ReadBytes((int)br.ReadUInt32()));
-            }
-            else if (this.Type == EntryType.Compressed)
-            {
-                this.Data = Compression.DecompressGZip(br.ReadBytes((int)this.CompressedSize));
-            }
-            else if (this.Type == EntryType.Uncompressed)
-            {
-                this.Data = br.ReadBytes((int)this.UncompressedSize);
-            }
-        }
-
-        public void Extract(string directoryLocation, bool identifyFile)
-        {
-            if (!Directory.Exists(directoryLocation))
-            {
-                Directory.CreateDirectory(directoryLocation);
-            }
-
-            if (identifyFile)
-            {
-                File.WriteAllBytes(string.Format("{0}//{1}.{2}", directoryLocation, Utilities.ByteArrayToHex(this.XXHash), this.GetEntryExtension()), this.Data);
+                return Compression.DecompressGZip(dataBuffer);
             }
             else
             {
-                File.WriteAllBytes(string.Format("{0}//{1}", directoryLocation, Utilities.ByteArrayToHex(this.XXHash)), this.Data);
-            }
-        }
-
-        public ExtensionType GetEntryExtensionType()
-        {
-            if (this.Type != EntryType.String)
-            {
-                byte[] id = new byte[8];
-                Buffer.BlockCopy(this.Data, 0, id, 0, 8);
-
-                if (id[0] == 'r' && id[1] == '3' && id[2] == 'd' && id[3] == '2')
-                {
-                    if (id[4] == 'M' && id[5] == 'e' && id[6] == 's' && id[7] == 'h')
-                    {
-                        return ExtensionType.SCB;
-                    }
-                    else if (id[4] == 's' && id[5] == 'k' && id[6] == 'l' && id[7] == 't')
-                    {
-                        return ExtensionType.SKL;
-                    }
-                    else if (id[4] == 'a' && id[5] == 'n' && id[6] == 'm' && id[7] == 'd')
-                    {
-                        return ExtensionType.ANM;
-                    }
-                    else if (id[4] == 'c' && id[5] == 'a' && id[6] == 'n' && id[7] == 'm')
-                    {
-                        return ExtensionType.ANM;
-                    }
-                }
-                else if (id[0] == 'B' && id[1] == 'K' && id[2] == 'H' && id[3] == 'D')
-                {
-                    return ExtensionType.BNK;
-                }
-                else if (id[0] == 0x33 && id[1] == 0x22 && id[2] == 0x11 && id[3] == 0x00)
-                {
-                    return ExtensionType.SKN;
-                }
-                else if (id[0] == 'D' && id[1] == 'D' && id[2] == 'S' && id[3] == 0x20)
-                {
-                    return ExtensionType.DDS;
-                }
-                else if (id[0] == 'P' && id[1] == 'R' && id[2] == 'O' && id[3] == 'P')
-                {
-                    return ExtensionType.BIN;
-                }
-                else if (id[0] == '[' && id[1] == 'O' && id[2] == 'b' && id[3] == 'j')
-                {
-                    return ExtensionType.SCO;
-                }
-                else if (BitConverter.ToInt32(id.Take(4).ToArray(), 0) == this.Data.Length)
-                {
-                    return ExtensionType.SKL;
-                }
-            }
-
-            return ExtensionType.Unknown;
-        }
-
-        public string GetEntryExtension()
-        {
-            ExtensionType extensionType = GetEntryExtensionType();
-            switch (extensionType)
-            {
-                case ExtensionType.ANM:
-                    return "anm";
-                case ExtensionType.BIN:
-                    return "bin";
-                case ExtensionType.BNK:
-                    return "bnk";
-                case ExtensionType.DDS:
-                    return "dds";
-                case ExtensionType.SCB:
-                    return "scb";
-                case ExtensionType.SCO:
-                    return "sco";
-                case ExtensionType.SKL:
-                    return "skl";
-                case ExtensionType.SKN:
-                    return "skn";
-                default:
-                    return "";
+                return dataBuffer;
             }
         }
     }
@@ -213,19 +126,6 @@ namespace Fantome.Libraries.League.IO.WAD
         /// <summary>
         /// The Data of this <see cref="WADEntry"/> is a file redirection
         /// </summary>
-        String
-    }
-
-    public enum ExtensionType
-    {
-        Unknown,
-        ANM,
-        BIN,
-        BNK,
-        DDS,
-        SCB,
-        SCO,
-        SKL,
-        SKN
+        FileRedirection
     }
 }
--- Fantome.League/IO/WAD/WADFile.cs
+++ Fantome.League/IO/WAD/WADFile.cs
@@ -2,6 +2,7 @@ using Fantome.Libraries.League.Helpers;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,16 +12,22 @@ namespace Fantome.Libraries.League.IO.WAD
     /// Represents a WAD File
     /// </summary>
     /// <remarks>WAD Files can only be read</remarks>
-    public class WADFile
+    public class WADFile : IDisposable
     {
         /// <summary>
         /// ECDSA Signature contained in the header of the file
         /// </summary>
         public byte[] ECDSA { get; private set; }
+
         /// <summary>
         /// A collection of <see cref="WADEntry"/>
         /// </summary>
         public List<WADEntry> Entries { get; private set; } = new List<WADEntry>();
+
+        /// <summary>
+        /// <see cref="Stream"/> of the currently opened <see cref="WADFile"/>.
+        /// </summary>
+        internal Stream _stream { get; private set; }
 
         /// <summary>
         /// Reads a <see cref="WADFile"/> from the specified location
@@ -34,7 +41,8 @@ namespace Fantome.Libraries.League.IO.WAD
         /// <param name="stream">The stream to read from</param>
         public WADFile(Stream stream)
         {
-            using (BinaryReader br = new BinaryReader(stream))
+            _stream = stream;
+            using (BinaryReader br = new BinaryReader(stream, Encoding.ASCII, true))
             {
                 string magic = Encoding.ASCII.GetString(br.ReadBytes(2));
                 if (magic != "RW")
@@ -66,33 +74,18 @@ namespace Fantome.Libraries.League.IO.WAD
 
                 for (int i = 0; i < fileCount; i++)
                 {
-                    Entries.Add(new WADEntry(br, major, minor));
-                }
-                foreach (WADEntry entry in Entries)
-                {
-                    entry.ReadData(br);
+                    Entries.Add(new WADEntry(this, br, major, minor));
                 }
             }
         }
 
-        public void Extract(string directoryLocation, bool identifyFiles)
+        /// <summary>
+        /// Closes the opened <see cref="Stream"/> of the current <see cref="WADFile"/> instance.
+        /// </summary>
+        public void Dispose()
         {
-            if (!Directory.Exists(directoryLocation))
-            {
-                Directory.CreateDirectory(directoryLocation);
-            }
-            //Using Parallel Foreach loop to make extraction super duper fast
-            Parallel.ForEach(this.Entries, entry =>
-            {
-                if (identifyFiles)
-                {
-                    File.WriteAllBytes(string.Format("{0}//{1}.{2}", directoryLocation, Utilities.ByteArrayToHex(entry.XXHash), entry.GetEntryExtension()), entry.Data);
-                }
-                else
-                {
-                    File.WriteAllBytes(string.Format("{0}//{1}", directoryLocation, Utilities.ByteArrayToHex(entry.XXHash)), entry.Data);
-                }
-            });
+            _stream?.Close();
+            _stream = null;
         }
     }
 }
