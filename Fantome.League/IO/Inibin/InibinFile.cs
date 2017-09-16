9f546d0082330d804e485c0ce418644c92540de1
Crauzer <filip.quitko@gmail.com>
2017-09-16 08:38:33 +0200

    Inibin Documentation

--- Fantome.League/IO/Inibin/InibinFile.cs
+++ Fantome.League/IO/Inibin/InibinFile.cs
@@ -5,19 +5,40 @@ using System.IO;
 
 namespace Fantome.Libraries.League.IO.Inibin
 {
+    /// <summary>
+    /// Represents binary ini files such as Troybin, Inibin and Cfgbin
+    /// </summary>
     public class InibinFile
     {
+        /// <summary>
+        /// Sections of this <see cref="InibinFile"/> separated by value type
+        /// </summary>
         public Dictionary<InibinFlags, InibinSet> Sets { get; private set; } = new Dictionary<InibinFlags, InibinSet>();
 
+        /// <summary>
+        /// Initializes a blank <see cref="InibinFile"/>
+        /// </summary>
         public InibinFile() { }
 
+        /// <summary>
+        /// Initializes a new <see cref="InibinFile"/> with the specified sets
+        /// </summary>
+        /// <param name="sets">Sets to add</param>
         public InibinFile(Dictionary<InibinFlags, InibinSet> sets)
         {
             this.Sets = sets;
         }
 
+        /// <summary>
+        /// Initializes a new <see cref="InibinFile"/> from the specified location
+        /// </summary>
+        /// <param name="fileLocation">The location to read from</param>
         public InibinFile(string fileLocation) : this(File.OpenRead(fileLocation)) { }
 
+        /// <summary>
+        /// Initializes a new <see cref="InibinFile"/> from a <see cref="Stream"/>
+        /// </summary>
+        /// <param name="stream">The <see cref="Stream"/> to read from</param>
         public InibinFile(Stream stream)
         {
             using (BinaryReader br = new BinaryReader(stream))
@@ -99,11 +120,19 @@ namespace Fantome.Libraries.League.IO.Inibin
             }
         }
 
+        /// <summary>
+        /// Writes this <see cref="InibinFile"/> to the specified location
+        /// </summary>
+        /// <param name="fileLocation">The location to write to</param>
         public void Write(string fileLocation)
         {
             Write(File.Create(fileLocation));
         }
 
+        /// <summary>
+        /// Writes this <see cref="InibinFile"/> to the specified <see cref="Stream"/>
+        /// </summary>
+        /// <param name="stream">The <see cref="Stream"/> to write to</param>
         public void Write(Stream stream)
         {
             using (BinaryWriter bw = new BinaryWriter(stream))
@@ -181,11 +210,22 @@ namespace Fantome.Libraries.League.IO.Inibin
             }
         }
 
+        /// <summary>
+        /// Adds a value to this <see cref="InibinFile"/>
+        /// </summary>
+        /// <param name="section">Section of the value being added</param>
+        /// <param name="property">Name of the value being added</param>
+        /// <param name="value">Value to add</param>
         public void AddValue(string section, string property, object value)
         {
             AddValue(Cryptography.SectionHash(section, property), value);
         }
 
+        /// <summary>
+        /// Adds a value to this <see cref="InibinFile"/>
+        /// </summary>
+        /// <param name="hash">Hash of the value being added</param>
+        /// <param name="value">Value to add</param>
         public void AddValue(uint hash, object value)
         {
             InibinFlags valueType;
@@ -284,36 +324,97 @@ namespace Fantome.Libraries.League.IO.Inibin
         }
     }
 
+    /// <summary>
+    /// Represents an <see cref="Exception"/> that's thrown when the user is trying to add an unsupported value type to an <see cref="InibinFile"/>
+    /// </summary>
     public class UnsupportedValueTypeException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="UnsupportedValueTypeException"/> with the specified error
+        /// </summary>
+        /// <param name="message">Message that describes the error</param>
         public UnsupportedValueTypeException(string message) : base(message) { }
     }
 
+    /// <summary>
+    /// Represents an <see cref="Exception"/> that's thrown when a <see cref="InibinFlags.FixedPointFloatList"/> value is overflowed
+    /// </summary>
     public class FixedPointFloatOverflowException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="FixedPointFloatOverflowException"/>
+        /// </summary>
         public FixedPointFloatOverflowException() : base("The Fixed Point Float value has to be between 0.0 and 25.5") { }
     }
 
+    /// <summary>
+    /// Represents an <see cref="Exception"/> that's thrown when a user tries to add a vector of unsupported size
+    /// </summary>
     public class VectorOverflowException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="VectorOverflowException"/>
+        /// </summary>
         public VectorOverflowException() : base("A Vector can only have the size of 2, 3, and 4") { }
     }
 
+    /// <summary>
+    /// Represents Value Types inside a <see cref="InibinFile"/>
+    /// </summary>
     [Flags]
     public enum InibinFlags : ushort
     {
+        /// <summary>
+        /// Represents an <see cref="int"/> value
+        /// </summary>
         Int32List = 1,
+        /// <summary>
+        /// Represents a <see cref="float"/> value
+        /// </summary>
         Float32List = 1 << 1,
+        /// <summary>
+        /// Represents a <see cref="double"/> value with the range of 0.0 - 25.5
+        /// </summary>
         FixedPointFloatList = 1 << 2,
+        /// <summary>
+        /// Represents a <see cref="short"/> value
+        /// </summary>
         Int16List = 1 << 3,
+        /// <summary>
+        /// Represents a <see cref="byte"/> value
+        /// </summary>
         Int8List = 1 << 4,
+        /// <summary>
+        /// Represents a <see cref="bool"/> value
+        /// </summary>
         BitList = 1 << 5,
+        /// <summary>
+        /// Represents a <see cref="byte"/> Vector3 value
+        /// </summary>
         FixedPointFloatListVec3 = 1 << 6,
+        /// <summary>
+        /// Represents a <see cref="float"/> Vector3 value
+        /// </summary>
         Float32ListVec3 = 1 << 7,
+        /// <summary>
+        /// Represents a <see cref="byte"/> Vector2 value
+        /// </summary>
         FixedPointFloatListVec2 = 1 << 8,
+        /// <summary>
+        /// Represents a <see cref="float"/> Vector2 value
+        /// </summary>
         Float32ListVec2 = 1 << 9,
+        /// <summary>
+        /// Represents a <see cref="byte"/> Vector4 value
+        /// </summary>
         FixedPointFloatListVec4 = 1 << 10,
+        /// <summary>
+        /// Represents a <see cref="float"/> Vector4 value
+        /// </summary>
         Float32ListVec4 = 1 << 11,
+        /// <summary>
+        /// Represents a <see cref="string"/> value
+        /// </summary>
         StringList = 1 << 12
     }
 }
