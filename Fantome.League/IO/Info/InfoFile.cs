48377657fadc82ddb8c11caf7edbd402b1e3c2ba
Crauzer <filip.quitko@gmail.com>
2017-11-04 10:55:13 +0100

    Info File

---
+++ Fantome.League/IO/Info/InfoFile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Fantome.Libraries.League.IO.InfoFile
+{
+    public class InfoFile
+    {
+        public List<InfoFileCharacter> Characters = new List<InfoFileCharacter>();
+
+        public InfoFile(string fileLocation) : this(File.OpenRead(fileLocation)) { }
+
+        public InfoFile(Stream stream)
+        {
+            using (BinaryReader br = new BinaryReader(stream))
+            {
+                uint characterCount = br.ReadUInt32();
+
+                for (int i = 0; i < characterCount; i++)
+                {
+                    this.Characters.Add(new InfoFileCharacter(br));
+                }
+            }
+        }
+    }
+}
---
+++ Fantome.League/IO/Info/InfoFileCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Fantome.Libraries.League.IO.InfoFile
+{
+    public class InfoFileCharacter
+    {
+        public string Name { get; set; }
+        public uint Unknown { get; set; }
+        public List<Tuple<string, uint>> Skins = new List<Tuple<string, uint>>();
+
+        public InfoFileCharacter(BinaryReader br)
+        {
+            this.Name = Encoding.ASCII.GetString(br.ReadBytes(br.ReadInt32()));
+            uint skinCount = br.ReadUInt32();
+            this.Unknown = br.ReadUInt32();
+
+            for (int i = 0; i < skinCount; i++)
+            {
+                if ((this.Skins.Count + 1) != skinCount)
+                {
+                    this.Skins.Add(new Tuple<string, uint>(Encoding.ASCII.GetString(br.ReadBytes(br.ReadInt32())), br.ReadUInt32()));
+                }
+                else
+                {
+                    this.Skins.Add(new Tuple<string, uint>(Encoding.ASCII.GetString(br.ReadBytes(br.ReadInt32())), 0));
+                }
+            }
+        }
+    }
+}
