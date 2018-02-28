e4a2a296387c2fad2dfd2fe7a055ccc1a05f6c2a
Crauzer <filip.quitko@gmail.com>
2018-02-28 15:11:00 +0100

    Better LightGrid doc

--- Fantome.League/IO/LightGrid/LightGridFile.cs
+++ Fantome.League/IO/LightGrid/LightGridFile.cs
@@ -7,16 +7,30 @@ namespace Fantome.Libraries.League.IO.LightGrid
 {
     public class LightGridFile
     {
+        private uint _headerVersion = 1;
         public uint Width { get; private set; }
-        public uint Heigth { get; private set; }
-        public float XBound { get; private set; }
-        public float YBound { get; private set; }
-        public LightGridSun Sun { get; private set; }
-        public List<ColorRGBAVector4Byte[]> Lights { get; private set; } = new List<ColorRGBAVector4Byte[]>();
+        public uint Height { get; private set; }
+        public Vector2 BoundsMax { get; set; }
+        public float CharacterIntensity { get; set; }
+        public float CharacterFullBrightIntensity { get; set; }
+        public float Unknown2 { get; set; }
+        public float Unknown3 { get; set; }
+        public float Unknown4 { get; set; }
+        public float Unknown5 { get; set; }
+        public float Unknown6 { get; set; }
+        public float Unknown7 { get; set; }
+        public float Unknown8 { get; set; }
+        public float Unknown9 { get; set; }
+        public float Unknown10 { get; set; }
+        public float Unknown11 { get; set; }
+        public float Unknown12 { get; set; }
+        public List<ColorRGBAVector4Byte[]> Grid { get; private set; } = new List<ColorRGBAVector4Byte[]>();
 
-        public LightGridFile(string fileLocation)
+        public LightGridFile(string fileLocation) : this(File.OpenRead(fileLocation)) { }
+
+        public LightGridFile(Stream stream)
         {
-            using (BinaryReader br = new BinaryReader(File.OpenRead(fileLocation)))
+            using (BinaryReader br = new BinaryReader(stream))
             {
                 uint version = br.ReadUInt32();
                 if (version != 3)
@@ -26,16 +40,33 @@ namespace Fantome.Libraries.League.IO.LightGrid
 
                 uint gridOffset = br.ReadUInt32();
                 this.Width = br.ReadUInt32();
-                this.Heigth = br.ReadUInt32();
-                this.XBound = br.ReadSingle();
-                this.YBound = br.ReadSingle();
-                this.Sun = new LightGridSun(br);
+                this.Height = br.ReadUInt32();
+                this.BoundsMax = new Vector2(br);
+                this.CharacterIntensity = br.ReadSingle();
+                this.CharacterFullBrightIntensity = br.ReadSingle();
+
+                if (gridOffset == 76)
+                {
+                    this._headerVersion = 2;
+
+                    this.Unknown2 = br.ReadSingle();
+                    this.Unknown3 = br.ReadSingle();
+                    this.Unknown4 = br.ReadSingle();
+                    this.Unknown5 = br.ReadSingle();
+                    this.Unknown6 = br.ReadSingle();
+                    this.Unknown7 = br.ReadSingle();
+                    this.Unknown8 = br.ReadSingle();
+                    this.Unknown9 = br.ReadSingle();
+                    this.Unknown10 = br.ReadSingle();
+                    this.Unknown11 = br.ReadSingle();
+                    this.Unknown12 = br.ReadSingle();
+                }
 
                 br.BaseStream.Seek(gridOffset, SeekOrigin.Begin);
-                uint cellCount = this.Width * this.Heigth;
+                uint cellCount = this.Width * this.Height;
                 for (int i = 0; i < cellCount; i++)
                 {
-                    this.Lights.Add(new ColorRGBAVector4Byte[]
+                    this.Grid.Add(new ColorRGBAVector4Byte[]
                         {
                             new ColorRGBAVector4Byte(br), new ColorRGBAVector4Byte(br), new ColorRGBAVector4Byte(br),
                             new ColorRGBAVector4Byte(br), new ColorRGBAVector4Byte(br), new ColorRGBAVector4Byte(br)
@@ -44,19 +75,34 @@ namespace Fantome.Libraries.League.IO.LightGrid
             }
         }
 
-        public void Write(string fileLocation)
+        public void Write(Stream stream)
         {
-            using (BinaryWriter bw = new BinaryWriter(File.Open(fileLocation, FileMode.Create)))
+            using (BinaryWriter bw = new BinaryWriter(stream))
             {
                 bw.Write((uint)3);
-                bw.Write((uint)76);
+                bw.Write(this._headerVersion == 1 ? 76 : 32);
                 bw.Write(this.Width);
-                bw.Write(this.Heigth);
-                bw.Write(this.XBound);
-                bw.Write(this.YBound);
-                this.Sun.Write(bw);
+                bw.Write(this.Height);
+                this.BoundsMax.Write(bw);
+                bw.Write(this.CharacterIntensity);
+                bw.Write(this.CharacterFullBrightIntensity);
 
-                foreach (ColorRGBAVector4Byte[] cell in this.Lights)
+                if (this._headerVersion == 2)
+                {
+                    bw.Write(this.Unknown2);
+                    bw.Write(this.Unknown3);
+                    bw.Write(this.Unknown4);
+                    bw.Write(this.Unknown5);
+                    bw.Write(this.Unknown6);
+                    bw.Write(this.Unknown7);
+                    bw.Write(this.Unknown8);
+                    bw.Write(this.Unknown9);
+                    bw.Write(this.Unknown10);
+                    bw.Write(this.Unknown11);
+                    bw.Write(this.Unknown12);
+                }
+
+                foreach (ColorRGBAVector4Byte[] cell in this.Grid)
                 {
                     for (int i = 0; i < 6; i++)
                     {
@@ -64,6 +110,11 @@ namespace Fantome.Libraries.League.IO.LightGrid
                     }
                 }
             }
+        }
+
+        public void Write(string fileLocation)
+        {
+            Write(File.OpenWrite(fileLocation));
         }
 
         public void WriteTexture(string fileLocation)
@@ -77,11 +128,11 @@ namespace Fantome.Libraries.League.IO.LightGrid
                 bw.Write((byte)0); //ColorMap Depth
                 bw.Write((uint)0); //X and Y Origin
                 bw.Write((ushort)this.Width);
-                bw.Write((ushort)this.Heigth);
+                bw.Write((ushort)this.Height);
                 bw.Write((byte)32); //Bits Per Color
                 bw.Write((byte)0); //Image Descriptor
 
-                foreach (ColorRGBAVector4Byte[] cell in this.Lights)
+                foreach (ColorRGBAVector4Byte[] cell in this.Grid)
                 {
                     for (int i = 0; i < 6; i++)
                     {
