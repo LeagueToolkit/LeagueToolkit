a87e413107c1b4a846e5cc91dd909af23831a9c6
Crauzer <filip.quitko@gmail.com>
2018-03-03 21:27:24 +0100

    Cell Flags

--- Fantome.League/IO/NavigationGrid/NavigationGridCell.cs
+++ Fantome.League/IO/NavigationGrid/NavigationGridCell.cs
@@ -20,7 +20,7 @@ namespace Fantome.Libraries.League.IO.NavigationGrid
         public float RefHintWeight { get; set; } = 0.5f;
         public ushort Unknown2 { get; private set; }
         public ushort ArrivalDirection { get; private set; } = 9;
-        public ushort Flags { get; set; }
+        public NavigationGridCellFlags Flags { get; set; }
         public short[] RefHintNodes { get; private set; } = new short[2] { -1, -1 };
 
         public NavigationGridCell(BinaryReader br, byte version)
@@ -41,7 +41,7 @@ namespace Fantome.Libraries.League.IO.NavigationGrid
                 this.GoodCellSessionID = br.ReadInt32();
                 this.RefHintWeight = br.ReadSingle();
                 this.ArrivalDirection = br.ReadUInt16();
-                this.Flags = br.ReadUInt16();
+                this.Flags = (NavigationGridCellFlags)br.ReadUInt16();
                 for (int i = 0; i < 2; i++)
                 {
                     this.RefHintNodes[i] = br.ReadInt16();
@@ -64,5 +64,19 @@ namespace Fantome.Libraries.League.IO.NavigationGrid
                 }
             }
         }
+    }
+
+    public enum NavigationGridCellFlags : ushort
+    {
+        CELL_HAS_GRASS = 0x1,
+        CELL_NOT_PASSABLE = 0x2,
+        CELL_BUSY = 0x4,
+        CELL_TARGETTED = 0x8,
+        CELL_MARKED = 0x10,
+        CELL_PATHED_ON = 0x20,
+        CELL_SEE_THROUGH = 0x40,
+        CELL_OTHERDIRECTION_END_TO_START = 0x80,
+        CELL_HAS_ANTI_BRUSH = 0x100,
+        CELL_HAS_TRANSPARENTTERRAIN = 0x42,
     }
 }
