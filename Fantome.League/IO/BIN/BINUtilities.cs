c6ae0cf4deeb539ff2a026b874519a7c23e72d82
Crauzer <filip.quitko@gmail.com>
2019-12-14 07:05:51 +0100

    Create BINUtilities.cs

---
+++ Fantome.League/IO/BIN/BINUtilities.cs
@@ -0,0 +1,28 @@
+﻿namespace Fantome.Libraries.League.IO.BIN
+{
+    internal class BINUtilities
+    {
+        private const int COMPLEX_TYPE_FLAG = 128;
+
+        public static BINValueType PackType(BINValueType type)
+        {
+            if ((int)type >= 18 && (int)type <= 23)
+            {
+                type = (BINValueType)(((int)type - 18) | COMPLEX_TYPE_FLAG);
+            }
+
+            return type;
+        }
+
+        public static BINValueType UnpackType(BINValueType type)
+        {
+            if (((int)type & COMPLEX_TYPE_FLAG) == COMPLEX_TYPE_FLAG)
+            {
+                type -= COMPLEX_TYPE_FLAG;
+                type += 18;
+            }
+
+            return type;
+        }
+    }
+}
