03360a0cef8af06a4014d6b267f0fe121ce3ef9b
Crauzer <filip.quitko@gmail.com>
2019-12-15 13:40:47 +0100

    bin

---
+++ Fantome.Libraries.League/IO/BIN/BINMap.cs
@@ -0,0 +1,102 @@
+﻿using Fantome.Libraries.League.Helpers.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Fantome.Libraries.League.IO.BIN
+{
+    public class BINMap : IBINValue, IEquatable<BINMap>
+    {
+        public IBINValue Parent { get; private set; }
+        public BINValueType KeyType { get; private set; }
+        public BINValueType ValueType { get; private set; }
+        public Dictionary<BINValue, BINValue> Values { get; private set; } = new Dictionary<BINValue, BINValue>();
+        public BINValue this[uint hash] => throw new NotImplementedException();
+        public BINValue this[string path] => throw new NotImplementedException();
+        public BINValue this[BINValue value]
+        {
+            get
+            {
+                return this.Values[value];
+            }
+        }
+
+        public BINMap(IBINValue parent, BINValueType keyType, BINValueType valueType, Dictionary<BINValue, BINValue> values)
+        {
+            this.Parent = parent;
+            this.KeyType = keyType;
+            this.ValueType = valueType;
+            this.Values = values;
+        }
+
+        public BINMap(BinaryReader br, IBINValue parent)
+        {
+            this.Parent = parent;
+
+            this.KeyType = (BINValueType)br.ReadByte();
+            this.ValueType = BINUtilities.UnpackType((BINValueType)br.ReadByte());
+            uint size = br.ReadUInt32();
+            uint valueCount = br.ReadUInt32();
+
+            for (int i = 0; i < valueCount; i++)
+            {
+                this.Values.Add(new BINValue(br, this, this.KeyType), new BINValue(br, this, this.ValueType));
+            }
+        }
+
+        public void Write(BinaryWriter bw)
+        {
+            bw.Write((byte)this.KeyType);
+            bw.Write((byte)BINUtilities.PackType(this.ValueType));
+            bw.Write(GetContentSize());
+            bw.Write(this.Values.Count);
+
+            foreach (KeyValuePair<BINValue, BINValue> pair in this.Values)
+            {
+                pair.Key.Write(bw, false);
+                pair.Value.Write(bw, false);
+            }
+        }
+
+        public uint GetContentSize()
+        {
+            uint size = 4;
+            foreach (KeyValuePair<BINValue, BINValue> pair in this.Values)
+            {
+                size += pair.Key.GetSize() + pair.Value.GetSize();
+            }
+            return size;
+        }
+
+        public uint GetSize()
+        {
+            return 1 + 1 + 4 + GetContentSize();
+        }
+
+        public string GetPath(bool excludeEntry = true)
+        {
+            return this.Parent.GetPath(excludeEntry);
+        }
+
+        public bool Equals(BINMap other)
+        {
+            if (this.KeyType != other.KeyType || this.ValueType != other.ValueType || this.Values.Count != other.Values.Count)
+            {
+                return false;
+            }
+            else
+            {
+                foreach (KeyValuePair<BINValue, BINValue> pair in this.Values)
+                {
+                    if (!other.Values.ContainsKey(pair.Key) || !other.Values.ContainsValue(pair.Value))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
---
+++ Fantome.Libraries.League/IO/BIN/BINStructure.cs
@@ -0,0 +1,113 @@
+﻿using Fantome.Libraries.League.Helpers.BIN;
+using Fantome.Libraries.League.Helpers.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Fantome.Libraries.League.IO.BIN
+{
+    public class BINStructure : IBINValue, IEquatable<BINStructure>
+    {
+        public IBINValue Parent { get; private set; }
+        public uint Property { get; private set; }
+        public List<BINValue> Values { get; private set; } = new List<BINValue>();
+        public BINValue this[string path] => throw new NotImplementedException();
+        public BINValue this[uint hash]
+        {
+            get
+            {
+                return this.Values.Find(x => x.Property == hash);
+            }
+        }
+
+        public BINStructure(BinaryReader br, BINValue parent)
+        {
+            this.Parent = parent;
+
+            this.Property = br.ReadUInt32();
+            if (this.Property == 0)
+            {
+                return;
+            }
+            
+            uint size = br.ReadUInt32();
+            ushort valueCount = br.ReadUInt16();
+
+            for (int i = 0; i < valueCount; i++)
+            {
+                this.Values.Add(new BINValue(br, this));
+            }
+        }
+
+        public void Write(BinaryWriter bw)
+        {
+            bw.Write(this.Property);
+            if (this.Property == 0)
+            {
+                return;
+            }
+
+            bw.Write(GetContentSize());
+            bw.Write((ushort)this.Values.Count);
+
+            foreach (BINValue value in this.Values)
+            {
+                value.Write(bw, true);
+            }
+        }
+
+        public uint GetContentSize()
+        {
+            uint size = 2;
+            foreach (BINValue value in this.Values)
+            {
+                size += value.GetSize();
+            }
+            return size;
+        }
+
+        public uint GetSize()
+        {
+            return (this.Property == 0) ? 4 : 4 + 4 + GetContentSize();
+        }
+
+        public string GetPath(bool excludeEntry = true)
+        {
+            string path = "";
+
+            if(this.Parent.Parent is BINContainer)
+            {
+                BINContainer container = this.Parent.Parent as BINContainer;
+                path = string.Format("{0}/{1}[{2}]", this.Parent.Parent.GetPath(excludeEntry), BINGlobal.GetClass(this.Property), container.Values.IndexOf(this.Parent as BINValue));
+
+            }
+            else
+            {
+                path = string.Format("{0}/{1}", this.Parent.GetPath(excludeEntry), BINGlobal.GetClass(this.Property));
+            }
+
+            return path;
+        }
+
+        public bool Equals(BINStructure other)
+        {
+            if(this.Property != other.Property || this.Values.Count != other.Values.Count)
+            {
+                return false;
+            }
+            else
+            {
+                foreach (BINValue binValue in this.Values)
+                {
+                    if (!other.Values.Contains(binValue))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
