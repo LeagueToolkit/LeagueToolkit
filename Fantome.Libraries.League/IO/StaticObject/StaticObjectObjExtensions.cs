9394deff229638f59f4d8ea665de2da97074ecd4
Crauzer <filip.quitko@gmail.com>
2020-08-15 01:39:18 +0200

    Create StaticObjectObjExtensions.cs

---
+++ Fantome.Libraries.League/IO/StaticObject/StaticObjectObjExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Fantome.Libraries.League.Helpers.Structures;
+using Fantome.Libraries.League.IO.OBJ;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fantome.Libraries.League.IO.StaticObject
+{
+    public static class StaticObjectObjExtensions
+    {
+        public static OBJFile GetObj(this StaticObjectSubmesh submesh)
+        {
+            List<Vector3> vertices = new List<Vector3>(submesh.Vertices.Count);
+            List<Vector2> uvs = new List<Vector2>(submesh.Vertices.Count);
+            foreach(StaticObjectVertex vertex in submesh.Vertices)
+            {
+                vertices.Add(vertex.Position);
+                uvs.Add(vertex.UV);
+            }
+
+            return new OBJFile(vertices, submesh.Indices, uvs);
+        }
+    }
+}
