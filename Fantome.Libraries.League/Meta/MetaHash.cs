0df6df91ce9bf4f2f3632460aeeefe2fa41ef313
Crauzer <filip.quitko@gmail.com>
2020-10-18 09:31:42 +0200

    Add structs for some primitive meta types

---
+++ Fantome.Libraries.League/Meta/MetaHash.cs
@@ -0,0 +1,40 @@
+﻿using Fantome.Libraries.League.Helpers.Hashing;
+
+namespace Fantome.Libraries.League.Meta
+{
+    public struct MetaHash
+    {
+        public uint Hash { get; private set; }
+        public string Value { get; private set; }
+
+        public MetaHash(uint hash)
+        {
+            this.Hash = hash;
+            this.Value = string.Empty;
+        }
+        public MetaHash(string value)
+        {
+            this.Hash = Fnv1a.HashLower(value);
+            this.Value = value;
+        }
+
+        public override int GetHashCode()
+        {
+            return (int)this.Hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is MetaHash other && other.Hash == this.Hash;
+        }
+
+        public static implicit operator uint(MetaHash metaHash)
+        {
+            return metaHash.Hash;
+        }
+        public static implicit operator MetaHash(uint hash)
+        {
+            return new MetaHash(hash);
+        }
+    }
+}
