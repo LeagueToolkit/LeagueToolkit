c8126b28677b9129636289041c7b508e9c91fe79
Crauzer <filip.quitko@gmail.com>
2020-10-18 09:32:06 +0200

    Add WriteLineIndented extension methods for StreamWriter

--- Fantome.Libraries.League/Helpers/Extensions/StreamWriterExtensions.cs
+++ Fantome.Libraries.League/Helpers/Extensions/StreamWriterExtensions.cs
@@ -6,11 +6,30 @@ using System.Text;
 
 namespace Fantome.Libraries.League.Helpers.Extensions
 {
-    public static class StreamWriterExtensions
+    internal static class StreamWriterExtensions
     {
         public static void WriteColor(this StreamWriter writer, Color color, ColorFormat format)
         {
             writer.Write(color.ToString(format));
+        }
+
+        public static void WriteLineIndented(this StreamWriter writer, int indentationLevel, string format, params object[] arg)
+        {
+            for(int i = 0; i < indentationLevel; i++)
+            {
+                writer.Write("    ");
+            }
+
+            writer.WriteLine(format, arg);
+        }
+        public static void WriteLineIndented(this StreamWriter writer, int indentationLevel, string value)
+        {
+            for (int i = 0; i < indentationLevel; i++)
+            {
+                writer.Write("    ");
+            }
+
+            writer.WriteLine(value);
         }
     }
 }
