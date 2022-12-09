7e37afe8ba913bb5214acba0bc6e5589e1c01891
Crauzer <filip.quitko@gmail.com>
2022-12-09 17:44:16 +0100

    add FromVertices to R3DBox

--- LeagueToolkit/Helpers/Structures/R3DBox.cs
+++ LeagueToolkit/Helpers/Structures/R3DBox.cs
@@ -1,4 +1,5 @@
 ﻿using LeagueToolkit.Helpers.Extensions;
+using System.Collections.Generic;
 using System.IO;
 using System.Numerics;
 
@@ -43,6 +44,42 @@ namespace LeagueToolkit.Helpers.Structures
             this.Max = r3dBox.Max;
         }
 
+        public static R3DBox FromVertices(IEnumerable<Vector3> vertices)
+        {
+            Vector3 min = new(float.MaxValue, float.MaxValue, float.MaxValue);
+            Vector3 max = new(float.MinValue, float.MinValue, float.MinValue);
+
+            foreach (Vector3 vertex in vertices)
+            {
+                if (min.X > vertex.X)
+                {
+                    min.X = vertex.X;
+                }
+                if (min.Y > vertex.Y)
+                {
+                    min.Y = vertex.Y;
+                }
+                if (min.Z > vertex.Z)
+                {
+                    min.Z = vertex.Z;
+                }
+                if (max.X < vertex.X)
+                {
+                    max.X = vertex.X;
+                }
+                if (max.Y < vertex.Y)
+                {
+                    max.Y = vertex.Y;
+                }
+                if (max.Z < vertex.Z)
+                {
+                    max.Z = vertex.Z;
+                }
+            }
+
+            return new(min, max);
+        }
+
         /// <summary>
         /// Writes this <see cref="R3DBox"/> into a <see cref="BinaryWriter"/>
         /// </summary>
@@ -60,13 +97,16 @@ namespace LeagueToolkit.Helpers.Structures
         {
             return this.Max - this.Min;
         }
+
         public Vector3 GetCentralPoint()
         {
             return new Vector3(
                 0.5f * (this.Min.X + this.Max.X),
                 0.5f * (this.Min.Y + this.Max.Y),
-                0.5f * (this.Min.Z + this.Max.Z));
+                0.5f * (this.Min.Z + this.Max.Z)
+            );
         }
+
         public R3DSphere GetBoundingSphere()
         {
             Vector3 centralPoint = GetCentralPoint();
@@ -81,9 +121,12 @@ namespace LeagueToolkit.Helpers.Structures
         /// <returns>Wheter this <see cref="R3DBox"/> contains the <see cref="Vector3"/> <paramref name="point"/></returns>
         public bool ContainsPoint(Vector3 point)
         {
-            return (point.X >= this.Min.X) && (point.X <= this.Max.X)
-                && (point.Y >= this.Min.Y) && (point.Y <= this.Max.Y) 
-                && (point.Z >= this.Min.Z) && (point.Z <= this.Max.Z);
+            return (point.X >= this.Min.X)
+                && (point.X <= this.Max.X)
+                && (point.Y >= this.Min.Y)
+                && (point.Y <= this.Max.Y)
+                && (point.Z >= this.Min.Z)
+                && (point.Z <= this.Max.Z);
         }
     }
 }
