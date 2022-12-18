04f87e7b0e4b9ddf9785515be585d413cfca9a56
Crauzer <filip.quitko@gmail.com>
2022-12-18 22:07:14 +0100

    samplerdata docs

--- LeagueToolkit/IO/MapGeometryFile/MapGeometrySamplerData.cs
+++ LeagueToolkit/IO/MapGeometryFile/MapGeometrySamplerData.cs
@@ -7,10 +7,27 @@ using System.Text;
 
 namespace LeagueToolkit.IO.MapGeometryFile
 {
+    /// <summary>
+    /// Contains information for an arbitrary environment terrain mesh sampler
+    /// </summary>
     public struct MapGeometrySamplerData
     {
+        /// <summary>
+        /// The path of the texture to use for this sampler
+        /// </summary>
         public string Texture;
+
+        /// <summary>
+        /// The scale of UVs being used to sample from <see cref="Texture"/>
+        /// </summary>
         public Vector2 Scale;
+
+        /// <summary>
+        /// The offset of UVs being used to sample from <see cref="Texture"/>
+        /// </summary>
+        /// <remarks>
+        /// Applied after scaling
+        /// </remarks>
         public Vector2 Bias;
 
         public MapGeometrySamplerData()
