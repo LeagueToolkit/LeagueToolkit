f7450800792cdda3ca5d8553c8b401c5c2315be6
Crauzer <filip.quitko@gmail.com>
2022-12-27 15:21:57 +0100

    `VertexElement` documentation

--- src/LeagueToolkit/Core/Memory/VertexElement.cs
+++ src/LeagueToolkit/Core/Memory/VertexElement.cs
@@ -5,9 +5,11 @@ using System.IO;
 namespace LeagueToolkit.Core.Memory
 {
     /// <summary>
-    /// Describes a vertex element
+    /// Describes an element of a vertex stored in <see cref="VertexBuffer"/>
+    /// <br></br>
+    /// Please refer to <seealso href="https://learn.microsoft.com/en-us/windows/win32/api/d3d11/ns-d3d11-d3d11_input_element_desc">
+    /// the D3D11 description of an element</seealso> for more info
     /// </summary>
-    // https://learn.microsoft.com/en-us/windows/win32/api/d3d11/ns-d3d11-d3d11_input_element_desc
     // Riot::X3D::VERTEXELEMENT
     [DebuggerDisplay("{Name} - {Format}")]
     public readonly struct VertexElement : IEquatable<VertexElement>
@@ -28,9 +30,20 @@ namespace LeagueToolkit.Core.Memory
         public static readonly VertexElement TEXCOORD_6 = new(ElementName.Texcoord6, ElementFormat.XY_Float32);
         public static readonly VertexElement LIGHTMAP_UV = new(ElementName.LightmapUV, ElementFormat.XY_Float32);
 
+        /// <value>
+        /// The <seealso href="https://learn.microsoft.com/en-us/windows/win32/direct3dhlsl/dx-graphics-hlsl-semantics">
+        /// semantic name</seealso> of this element
+        /// </value>
         public ElementName Name { get; }
+
+        /// <summary>
+        /// The format of this element's value
+        /// </summary>
         public ElementFormat Format { get; }
 
+        /// <summary>
+        /// Creates a new <see cref="VertexElement"/> with the specified <paramref name="name"/> and <paramref name="format"/>
+        /// </summary>
         public VertexElement(ElementName name, ElementFormat format)
         {
             this.Name = name;
@@ -49,8 +62,10 @@ namespace LeagueToolkit.Core.Memory
             bw.Write((uint)this.Format);
         }
 
+        /// <returns>The size of an element's value</returns>
         public int GetSize() => GetFormatSize(this.Format);
 
+        /// <returns>The size of <paramref name="format"/></returns>
         public static int GetFormatSize(ElementFormat format)
         {
             return format switch
@@ -83,9 +98,12 @@ namespace LeagueToolkit.Core.Memory
         public override int GetHashCode() => HashCode.Combine(this.Name, this.Format);
     }
 
-    // The values of this enum are used as stream indices for the renderer.
-    // Elements should always be sorted by ascending order
-    // https://learn.microsoft.com/en-us/windows/win32/direct3dhlsl/dx-graphics-hlsl-semantics#vertex-shader-semantics
+    /// <summary>
+    /// The values of <see cref="ElementName"/> are used as Stream ID's for the renderer. <br></br>
+    /// <seealso href="https://learn.microsoft.com/en-us/windows/win32/direct3dhlsl/dx-graphics-hlsl-semantics#vertex-shader-semantics">
+    /// D3D HLSL Vertex Shader Semantics
+    /// </seealso>
+    /// </summary>
     // Riot::Renderer::Mesh::Elem
     public enum ElementName : int
     {
