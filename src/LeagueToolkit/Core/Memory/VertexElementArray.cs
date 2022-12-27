5caa6e44c04f689b3fb527ea76b8dcb56c0e8deb
Crauzer <filip.quitko@gmail.com>
2022-12-27 17:45:55 +0100

    Add `VertexElementArray` documentation

--- src/LeagueToolkit/Core/Memory/VertexElementArray.cs
+++ src/LeagueToolkit/Core/Memory/VertexElementArray.cs
@@ -4,12 +4,24 @@ using System.Runtime.InteropServices;
 
 namespace LeagueToolkit.Core.Memory
 {
+    /// <summary>
+    /// Provides an read-only interface for indexing and enumerating vertex element values described by <see cref="VertexElementAccessor"/>
+    /// </summary>
+    /// <typeparam name="TElement">The type of the serialized element value</typeparam>
     public readonly struct VertexElementArray<TElement> : IReadOnlyList<TElement> where TElement : struct
     {
         public int Count => this.Accessor.VertexCount;
 
+        /// <summary>
+        /// The accessor which is used by the <see cref="VertexElementArray{TElement}"/>
+        /// to fetch element values from memory
+        /// </summary>
         public VertexElementAccessor Accessor { get; }
 
+        /// <summary>
+        /// Creates a new <see cref="VertexElementArray{TElement}"/>
+        /// </summary>
+        /// <param name="accessor">The accessor to use for fetching element values from memory</param>
         public VertexElementArray(VertexElementAccessor accessor)
         {
             this.Accessor = accessor;
