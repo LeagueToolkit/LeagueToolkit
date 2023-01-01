937630b18d0e541f4d399572788a927a816ede04
Crauzer <filip.quitko@gmail.com>
2023-01-01 14:07:33 +0100

    Add `InstancedVertexBufferView` docs

--- src/LeagueToolkit/Core/Memory/InstancedVertexBufferView.cs
+++ src/LeagueToolkit/Core/Memory/InstancedVertexBufferView.cs
@@ -4,11 +4,21 @@ using System.Linq;
 
 namespace LeagueToolkit.Core.Memory
 {
+    /// <summary>
+    /// Represents a view into an instanced/sharded vertex buffer by wrapping multiple vertex buffer views<br></br>
+    /// Refer to this <seealso href="https://www.braynzarsoft.net/viewtutorial/q16390-33-instancing-with-indexed-primitives">
+    /// blogpost on how instancing works in D3D11</seealso> for a deeper explanation
+    /// </summary>
     public readonly struct InstancedVertexBufferView : IInstancedVertexBufferView
     {
         public int VertexCount { get; }
         public IReadOnlyList<IVertexBufferView> Buffers { get; }
 
+        /// <summary>
+        /// Creates a new <see cref="InstancedVertexBufferView"/> object with the specified parameters
+        /// </summary>
+        /// <param name="vertexCount">The vertex count of all buffers</param>
+        /// <param name="buffers">The buffers to wrap</param>
         public InstancedVertexBufferView(int vertexCount, IReadOnlyList<IVertexBufferView> buffers)
         {
             Guard.IsGreaterThan(vertexCount, 0, nameof(vertexCount));
