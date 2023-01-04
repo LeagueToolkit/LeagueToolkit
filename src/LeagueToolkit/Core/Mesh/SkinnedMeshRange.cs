b6e13f442a395f5c70279364ee68a5665b05a4a2
Crauzer <filip.quitko@gmail.com>
2023-01-04 21:43:07 +0100

    Add `SkinnedMeshRange` docs

--- src/LeagueToolkit/Core/Mesh/SkinnedMeshRange.cs
+++ src/LeagueToolkit/Core/Mesh/SkinnedMeshRange.cs
@@ -3,14 +3,32 @@ using System.IO;
 
 namespace LeagueToolkit.Core.Mesh
 {
+    /// <summary>Represents a <see cref="SkinnedMesh"/> primitive</summary>
     public readonly struct SkinnedMeshRange
     {
+        /// <summary>Gets the primitive's material name</summary>
         public string Material { get; }
+
+        /// <summary>Gets the primitive's start vertex</summary>
         public int StartVertex { get; }
+
+        /// <summary>Gets the primitive's vertex count</summary>
         public int VertexCount { get; }
+
+        /// <summary>Gets the primitive's start index</summary>
         public int StartIndex { get; }
+
+        /// <summary>Gets the primitive's index count</summary>
         public int IndexCount { get; }
 
+        /// <summary>
+        /// Creates a new <see cref="SkinnedMeshRange"/> object with the specified parameters
+        /// </summary>
+        /// <param name="material">The material name of the <see cref="SkinnedMeshRange"/></param>
+        /// <param name="startVertex">The start vertex of the <see cref="SkinnedMeshRange"/></param>
+        /// <param name="vertexCount">The vertex count of the <see cref="SkinnedMeshRange"/></param>
+        /// <param name="startIndex">The start index of the <see cref="SkinnedMeshRange"/></param>
+        /// <param name="indexCount">The index count of the <see cref="SkinnedMeshRange"/></param>
         public SkinnedMeshRange(string material, int startVertex, int vertexCount, int startIndex, int indexCount)
         {
             this.Material = material;
