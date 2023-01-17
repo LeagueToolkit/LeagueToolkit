1901ae47f63ff72f75cc774cb0e84aec6a69e08c
Crauzer <filip.quitko@gmail.com>
2023-01-17 13:31:40 +0100

    Update Program.cs

--- src/LeagueToolkit.Sandbox/Program.cs
+++ src/LeagueToolkit.Sandbox/Program.cs
@@ -1,8 +1,8 @@
-﻿using CommunityToolkit.Diagnostics;
+﻿using BCnEncoder.Shared;
+using CommunityToolkit.Diagnostics;
 using CommunityToolkit.HighPerformance;
 using LeagueToolkit.Core.Memory;
 using LeagueToolkit.Core.Mesh;
-using LeagueToolkit.Core.Primitives;
 using LeagueToolkit.Core.Renderer;
 using LeagueToolkit.IO.AnimationFile;
 using LeagueToolkit.IO.MapGeometryFile;
@@ -14,27 +14,24 @@ using LeagueToolkit.IO.StaticObjectFile;
 using LeagueToolkit.Meta;
 using LeagueToolkit.Meta.Classes;
 using LeagueToolkit.Meta.Dump;
+using LeagueToolkit.Toolkit;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Numerics;
 using System.Reflection;
-using BCnEncoder.Shared;
-using LeagueToolkit.Toolkit;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
-using BCnEncoder.Decoder;
-using BCnEncoder.Shared.ImageFiles;
 using System.Threading.Tasks;
-using System.Net.Http;
 
 namespace LeagueToolkit.Sandbox
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             ProfileMapgeoToGltf();
 
@@ -68,9 +65,7 @@ namespace LeagueToolkit.Sandbox
 
         static void ProfileTexture()
         {
-            Texture texture = Texture.Load(
-                File.OpenRead("4x_canister_a.srt_preseason13_chemtech.dds")
-            );
+            Texture texture = Texture.Load(File.OpenRead("4x_canister_a.srt_preseason13_chemtech.dds"));
 
             ReadOnlyMemory2D<ColorRgba32> mipmap = texture.Mips[0];
 
@@ -117,14 +112,10 @@ namespace LeagueToolkit.Sandbox
             File.WriteAllBytes("hashes.bintypes.txt", binTypesBuffer);
             File.WriteAllBytes("hashes.binfields.txt", binFieldsBuffer);
 
-            IEnumerable<string> classes = File.ReadLines("hashes.bintypes.txt")
-                .Select(line => line.Split(' ')[1]);
-            IEnumerable<string> properties = File.ReadLines("hashes.binfields.txt")
-                .Select(line => line.Split(' ')[1]);
+            IEnumerable<string> classes = File.ReadLines("hashes.bintypes.txt").Select(line => line.Split(' ')[1]);
+            IEnumerable<string> properties = File.ReadLines("hashes.binfields.txt").Select(line => line.Split(' ')[1]);
 
-            MetaDump
-                .Deserialize(File.ReadAllText(metaJsonFile))
-                .WriteMetaClasses(outputFile, classes, properties);
+            MetaDump.Deserialize(File.ReadAllText(metaJsonFile)).WriteMetaClasses(outputFile, classes, properties);
         }
 #endif
 
@@ -153,12 +144,7 @@ namespace LeagueToolkit.Sandbox
                 .WithBakedTerrainSamplers(mgeo.BakedTerrainSamplers);
 
             Dictionary<ElementName, int> elementOrder =
-                new()
-                {
-                    { ElementName.DiffuseUV, 0 },
-                    { ElementName.Normal, 1 },
-                    { ElementName.Position, 2 }
-                };
+                new() { { ElementName.DiffuseUV, 0 }, { ElementName.Normal, 1 }, { ElementName.Position, 2 } };
 
             foreach (MapGeometryModel mesh in mgeo.Meshes)
             {
@@ -169,9 +155,7 @@ namespace LeagueToolkit.Sandbox
                         .OrderBy(element => element.Name),
                     mesh.VerticesView.VertexCount
                 );
-                var (indexBuffer, indexBufferWriter) = mapBuilder.UseIndexBuffer(
-                    mesh.Indices.Length
-                );
+                var (indexBuffer, indexBufferWriter) = mapBuilder.UseIndexBuffer(mesh.Indices.Length);
 
                 indexBufferWriter.Write(mesh.Indices.Span);
                 RewriteVertexBuffer(mesh, vertexBufferWriter);
@@ -188,11 +172,7 @@ namespace LeagueToolkit.Sandbox
                     .WithGeometry(
                         mesh.Submeshes.Select(
                             submesh =>
-                                new MeshPrimitiveBuilder(
-                                    submesh.Material,
-                                    submesh.StartIndex,
-                                    submesh.IndexCount
-                                )
+                                new MeshPrimitiveBuilder(submesh.Material, submesh.StartIndex, submesh.IndexCount)
                         ),
                         vertexBuffer,
                         indexBuffer
@@ -206,36 +186,23 @@ namespace LeagueToolkit.Sandbox
 
             static void RewriteVertexBuffer(MapGeometryModel mesh, VertexBufferWriter writer)
             {
-                bool hasPositions = mesh.VerticesView.TryGetAccessor(
-                    ElementName.Position,
-                    out var positionAccessor
-                );
-                bool hasNormals = mesh.VerticesView.TryGetAccessor(
-                    ElementName.Normal,
-                    out var normalAccessor
-                );
+                bool hasPositions = mesh.VerticesView.TryGetAccessor(ElementName.Position, out var positionAccessor);
+                bool hasNormals = mesh.VerticesView.TryGetAccessor(ElementName.Normal, out var normalAccessor);
                 bool hasBaseColor = mesh.VerticesView.TryGetAccessor(
                     ElementName.PrimaryColor,
                     out var baseColorAccessor
                 );
-                bool hasDiffuseUvs = mesh.VerticesView.TryGetAccessor(
-                    ElementName.DiffuseUV,
-                    out var diffuseUvAccessor
-                );
+                bool hasDiffuseUvs = mesh.VerticesView.TryGetAccessor(ElementName.DiffuseUV, out var diffuseUvAccessor);
                 bool hasLightmapUvs = mesh.VerticesView.TryGetAccessor(
                     ElementName.LightmapUV,
                     out var lightmapUvAccessor
                 );
 
                 if (hasPositions is false)
-                    ThrowHelper.ThrowInvalidOperationException(
-                        $"Mesh: {mesh.Name} does not have vertex positions"
-                    );
+                    ThrowHelper.ThrowInvalidOperationException($"Mesh: {mesh.Name} does not have vertex positions");
 
                 VertexElementArray<Vector3> positionsArray = positionAccessor.AsVector3Array();
-                VertexElementArray<Vector3> normalsArray = hasNormals
-                    ? normalAccessor.AsVector3Array()
-                    : new();
+                VertexElementArray<Vector3> normalsArray = hasNormals ? normalAccessor.AsVector3Array() : new();
                 var baseColorArray = hasBaseColor ? baseColorAccessor.AsBgraU8Array() : new();
                 VertexElementArray<Vector2> diffuseUvsArray = hasDiffuseUvs
                     ? diffuseUvAccessor.AsVector2Array()
