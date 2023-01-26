fc67f7b1d04e98235fbac7607a80913141e3000b
Crauzer <filip.quitko@gmail.com>
2023-01-26 11:44:37 +0100

    `JointBuilder` docs

--- src/LeagueToolkit/Core/Animation/Builders/JointBuiler.cs
+++ src/LeagueToolkit/Core/Animation/Builders/JointBuiler.cs
@@ -1,28 +1,67 @@
 ﻿using CommunityToolkit.Diagnostics;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Numerics;
 
 namespace LeagueToolkit.Core.Animation.Builders;
 
+/// <summary>
+/// Provides an interface for building a <see cref="Joint"/> object
+/// </summary>
 [DebuggerDisplay("{GetPath(), nq}", Name = "{Name}")]
 public sealed class JointBuilder
 {
+    /// <summary>
+    /// Gets or sets the name of the joint
+    /// </summary>
     public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the flags of the joint
+    /// </summary>
     public ushort Flags { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the joint is a skin influence
+    /// </summary>
     public bool IsInfluence { get; set; }
 
+    /// <summary>
+    /// Gets the parent of the joint
+    /// </summary>
+    /// <remarks>
+    /// Set to <see langword="null"/> if the joint has no parent
+    /// </remarks>
     public JointBuilder Parent { get; private set; }
 
+    /// <summary>
+    /// Gets or sets the radius of the joint
+    /// </summary>
     public float Radius { get; set; } = 2.1f;
 
+    /// <summary>
+    /// Gets or sets the local transform of the joint
+    /// </summary>
     public Matrix4x4 LocalTransform { get; set; }
+
+    /// <summary>
+    /// Gets or sets the inverse bind transform of the joint
+    /// </summary>
     public Matrix4x4 InverseBindTransform { get; set; }
 
+    /// <summary>
+    /// Gets the children of the joint
+    /// </summary>
     public IReadOnlyList<JointBuilder> Children => this._children;
     private readonly List<JointBuilder> _children = new();
 
+    /// <summary>
+    /// Creates a new <see cref="JointBuilder"/> object with the specified parameters
+    /// </summary>
+    /// <param name="name">The name of the joint</param>
+    /// <remarks>
+    /// This creates a root joint (without a parent)
+    /// </remarks>
     public JointBuilder(string name) : this(name, null) { }
 
     internal JointBuilder(string name, JointBuilder parent)
@@ -33,6 +72,10 @@ public sealed class JointBuilder
         this.Parent = parent;
     }
 
+    /// <summary>
+    /// Sets the name of the <see cref="JointBuilder"/>
+    /// </summary>
+    /// <param name="name">The name of the <see cref="JointBuilder"/></param>
     public JointBuilder WithName(string name)
     {
         Guard.IsNotNull(name, nameof(name));
@@ -42,6 +85,10 @@ public sealed class JointBuilder
         return this;
     }
 
+    /// <summary>
+    /// Sets the flags of the <see cref="JointBuilder"/>
+    /// </summary>
+    /// <param name="flags">The flags of the <see cref="JointBuilder"/></param>
     public JointBuilder WithFlags(ushort flags)
     {
         this.Flags = flags;
@@ -49,6 +96,10 @@ public sealed class JointBuilder
         return this;
     }
 
+    /// <summary>
+    /// Sets a value indicating whether the <see cref="JointBuilder"/> is a skin influence
+    /// </summary>
+    /// <param name="isInfluence">The value indicating whether the <see cref="JointBuilder"/> is a skin influence</param>
     public JointBuilder WithInfluence(bool isInfluence)
     {
         this.IsInfluence = isInfluence;
@@ -56,6 +107,10 @@ public sealed class JointBuilder
         return this;
     }
 
+    /// <summary>
+    /// Sets the radius of the <see cref="JointBuilder"/>
+    /// </summary>
+    /// <param name="radius">The radius of the <see cref="JointBuilder"/></param>
     public JointBuilder WithRadius(float radius)
     {
         this.Radius = radius;
@@ -63,6 +118,10 @@ public sealed class JointBuilder
         return this;
     }
 
+    /// <summary>
+    /// Sets the local transform of the <see cref="JointBuilder"/>
+    /// </summary>
+    /// <param name="localTransform">The local transform of the <see cref="JointBuilder"/></param>
     public JointBuilder WithLocalTransform(Matrix4x4 localTransform)
     {
         this.LocalTransform = localTransform;
@@ -70,6 +129,10 @@ public sealed class JointBuilder
         return this;
     }
 
+    /// <summary>
+    /// Sets the inverse bind transform of the <see cref="JointBuilder"/>
+    /// </summary>
+    /// <param name="inverseBindTransform">The inverse bind transform of the <see cref="JointBuilder"/></param>
     public JointBuilder WithInverseBindTransform(Matrix4x4 inverseBindTransform)
     {
         this.InverseBindTransform = inverseBindTransform;
@@ -77,6 +140,11 @@ public sealed class JointBuilder
         return this;
     }
 
+    /// <summary>
+    /// Creates a new <see cref="JointBuilder"/> and adds it to <see cref="Children"/>
+    /// </summary>
+    /// <param name="name">The name of the child <see cref="JointBuilder"/></param>
+    /// <returns>The created child <see cref="JointBuilder"/></returns>
     public JointBuilder CreateJoint(string name)
     {
         JointBuilder child = new(name, this);
@@ -86,6 +154,9 @@ public sealed class JointBuilder
         return child;
     }
 
+    /// <summary>
+    /// Enumerates all children <see cref="JointBuilder"/> instances recursively
+    /// </summary>
     public IEnumerable<JointBuilder> TraverseChildren()
     {
         foreach (JointBuilder joint in this._children)
