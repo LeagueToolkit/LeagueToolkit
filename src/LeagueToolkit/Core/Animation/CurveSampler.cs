a0b4a5a00495103200c89ba4d5ea1c193c23f6c6
Crauzer <filip.quitko@gmail.com>
2023-01-31 13:22:57 +0100

    move `ICurveSampler`

--- src/LeagueToolkit/Core/Animation/CurveSampler.cs
+++ src/LeagueToolkit/Core/Animation/CurveSampler.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace LeagueToolkit.Core.Animation;
 
-namespace LeagueToolkit.Core.Animation;
+public interface ICurveSampler<T>
+{
+    public T Sample(float time);
+}
 
 public static class CurveSampler
 {
