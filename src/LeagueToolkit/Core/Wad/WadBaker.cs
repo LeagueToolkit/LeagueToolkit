fd84c767a964d6c9ec19f19c438d6059bd331c45
Crauzer <filip.quitko@gmail.com>
2023-02-03 18:44:51 +0100

    start working on baker

---
+++ src/LeagueToolkit/Core/Wad/WadBaker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueToolkit.Core.Wad;
+
+public static class WadBaker
+{
+    public static void BakeDirectory(string path, WadBakeSettings settings)
+    {
+        BakeDirectory(new() { Path = path, Setings = settings });
+    }
+
+    private static void BakeDirectory(WadBakeContext context) { }
+}
+
+internal sealed class WadBakeContext
+{
+    public string Path { get; init; }
+    public WadBakeSettings Setings { get; init; }
+}
+
+public struct WadBakeSettings
+{
+    public bool DetectDuplicateChunkData { get; set; }
+
+    public WadBakeSettings()
+    {
+        this.DetectDuplicateChunkData = true;
+    }
+}
--- src/LeagueToolkit/Core/Wad/WadFile.cs
+++ src/LeagueToolkit/Core/Wad/WadFile.cs
@@ -1,131 +1,141 @@
 ﻿using CommunityToolkit.Diagnostics;
 using CommunityToolkit.HighPerformance;
 using CommunityToolkit.HighPerformance.Buffers;
+using Standart.Hash.xxHash;
 using System.IO.Compression;
 using System.Text;
 
-namespace LeagueToolkit.Core.Wad
+namespace LeagueToolkit.Core.Wad;
+
+public sealed class WadFile : IDisposable
 {
-    public sealed class WadFile : IDisposable
+    internal const int HEADER_SIZE_V3 = 272;
+
+    public IReadOnlyDictionary<ulong, WadChunk> Chunks => this._chunks;
+    private readonly Dictionary<ulong, WadChunk> _chunks;
+
+    private readonly FileStream _stream;
+
+    public bool IsDiposed { get; private set; }
+
+    public WadFile(string path)
     {
-        internal const int HEADER_SIZE_V3 = 272;
+        // Open wad stream and store it for later
+        this._stream = File.OpenRead(path);
+        using BinaryReader br = new(this._stream, Encoding.UTF8, true);
 
-        public IReadOnlyDictionary<ulong, WadChunk> Chunks => this._chunks;
-        private readonly Dictionary<ulong, WadChunk> _chunks;
+        string magic = Encoding.ASCII.GetString(br.ReadBytes(2));
+        if (magic is not "RW")
+            ThrowHelper.ThrowInvalidDataException($"Invalid file signature: {magic}");
 
-        private readonly FileStream _stream;
+        byte major = br.ReadByte();
+        byte minor = br.ReadByte();
+        if (major > 3)
+            ThrowHelper.ThrowInvalidDataException($"Invalid version: {major}.{minor}");
 
-        public bool IsDiposed { get; private set; }
+        ulong dataChecksum = 0; // probably not "dataChecksum"
 
-        public WadFile(string path)
+        if (major is 2)
         {
-            // Open wad stream and store it for later
-            this._stream = File.OpenRead(path);
-            using BinaryReader br = new(this._stream, Encoding.UTF8, true);
+            byte ecdsaLength = br.ReadByte();
+            br.BaseStream.Seek(83, SeekOrigin.Current); // ECDSA Signature
 
-            string magic = Encoding.ASCII.GetString(br.ReadBytes(2));
-            if (magic is not "RW")
-                ThrowHelper.ThrowInvalidDataException($"Invalid file signature: {magic}");
-
-            byte major = br.ReadByte();
-            byte minor = br.ReadByte();
-            if (major > 3)
-                ThrowHelper.ThrowInvalidDataException($"Invalid version: {major}.{minor}");
-
-            ulong dataChecksum = 0; // probably not "dataChecksum"
-
-            if (major is 2)
-            {
-                byte ecdsaLength = br.ReadByte();
-                br.BaseStream.Seek(83, SeekOrigin.Current); // ECDSA Signature
-
-                dataChecksum = br.ReadUInt64();
-            }
-            else if (major is 3)
-            {
-                br.BaseStream.Seek(256, SeekOrigin.Current); // ECDSA Signature
-                dataChecksum = br.ReadUInt64();
-            }
-
-            if (major is 1 or 2)
-            {
-                ushort tocStartOffset = br.ReadUInt16();
-                ushort tocFileEntrySize = br.ReadUInt16();
-            }
-
-            // Read chunks
-            int chunkCount = br.ReadInt32();
-            this._chunks = new(chunkCount);
-            for (int i = 0; i < chunkCount; i++)
-            {
-                WadChunk chunk = WadChunk.Read(br, major);
-
-                if (!this._chunks.TryAdd(chunk.PathHash, chunk))
-                    ThrowHelper.ThrowInvalidDataException(
-                        $"Tried to read a chunk which already exists: {chunk.PathHash}"
-                    );
-            }
+            dataChecksum = br.ReadUInt64();
+        }
+        else if (major is 3)
+        {
+            br.BaseStream.Seek(256, SeekOrigin.Current); // ECDSA Signature
+            dataChecksum = br.ReadUInt64();
         }
 
-        public MemoryOwner<byte> LoadChunk(ulong pathHash)
+        if (major is 1 or 2)
         {
-            WadChunk chunk = FindChunk(pathHash);
-
-            return LoadChunk(chunk);
+            ushort tocStartOffset = br.ReadUInt16();
+            ushort tocFileEntrySize = br.ReadUInt16();
         }
 
-        public MemoryOwner<byte> LoadChunk(WadChunk chunk)
+        // Read chunks
+        int chunkCount = br.ReadInt32();
+        this._chunks = new(chunkCount);
+        for (int i = 0; i < chunkCount; i++)
         {
-            MemoryOwner<byte> chunkDataOwner = MemoryOwner<byte>.Allocate(chunk.CompressedSize);
-            this._stream.Read(chunkDataOwner.Span);
+            WadChunk chunk = WadChunk.Read(br, major);
 
-            return chunkDataOwner;
+            if (!this._chunks.TryAdd(chunk.PathHash, chunk))
+                ThrowHelper.ThrowInvalidDataException($"Tried to read a chunk which already exists: {chunk.PathHash}");
+        }
+    }
+
+    public MemoryOwner<byte> LoadChunk(string path) => LoadChunk(xxHash64.ComputeHash(path.ToLower()));
+
+    public MemoryOwner<byte> LoadChunk(ulong pathHash)
+    {
+        WadChunk chunk = FindChunk(pathHash);
+
+        return LoadChunk(chunk);
+    }
+
+    public MemoryOwner<byte> LoadChunk(WadChunk chunk)
+    {
+        MemoryOwner<byte> chunkDataOwner = MemoryOwner<byte>.Allocate(chunk.CompressedSize);
+        this._stream.Read(chunkDataOwner.Span);
+
+        return chunkDataOwner;
+    }
+
+    public Stream OpenChunk(string path) => OpenChunk(xxHash64.ComputeHash(path.ToLower()));
+
+    public Stream OpenChunk(ulong pathHash)
+    {
+        WadChunk chunk = FindChunk(pathHash);
+
+        return OpenChunk(chunk);
+    }
+
+    public Stream OpenChunk(WadChunk chunk)
+    {
+        MemoryOwner<byte> chunkData = LoadChunk(chunk);
+
+        return chunk.CompressionType switch
+        {
+            WadChunkCompression.None => chunkData.AsStream(),
+            WadChunkCompression.GZip => new GZipStream(chunkData.AsStream(), CompressionMode.Decompress),
+            WadChunkCompression.Satellite
+                => throw new NotImplementedException("Opening satellite chunks is not supported"),
+            // zstd handles frames for us so we can ignore chunks
+            WadChunkCompression.Zstd
+            or WadChunkCompression.ZstdChunked
+                => new ZstdNet.DecompressionStream(chunkData.AsStream()),
+            _ => throw new InvalidOperationException($"Invalid chunk compression type: {chunk.CompressionType}")
+        };
+    }
+
+    public WadChunk FindChunk(string path) => FindChunk(xxHash64.ComputeHash(path.ToLower()));
+
+    public WadChunk FindChunk(ulong pathHash)
+    {
+        if (!this._chunks.TryGetValue(pathHash, out WadChunk chunk))
+            ThrowHelper.ThrowArgumentException(nameof(pathHash), $"Failed to find chunk: {pathHash}");
+
+        return chunk;
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (this.IsDiposed)
+            return;
+
+        if (disposing)
+        {
+            this._stream.Dispose();
         }
 
-        public Stream OpenChunk(ulong pathHash)
-        {
-            WadChunk chunk = FindChunk(pathHash);
-            MemoryOwner<byte> chunkData = LoadChunk(chunk);
+        this.IsDiposed = true;
+    }
 
-            return chunk.CompressionType switch
-            {
-                WadChunkCompression.None => chunkData.AsStream(),
-                WadChunkCompression.GZip => new GZipStream(chunkData.AsStream(), CompressionMode.Decompress),
-                WadChunkCompression.Satellite
-                    => throw new NotImplementedException("Opening satellite chunks is not supported"),
-                // zstd handles frames for us so we can ignore chunks
-                WadChunkCompression.Zstd
-                or WadChunkCompression.ZstdChunked
-                    => new ZstdNet.DecompressionStream(chunkData.AsStream()),
-                _ => throw new InvalidOperationException($"Invalid chunk compression type: {chunk.CompressionType}")
-            };
-        }
-
-        public WadChunk FindChunk(ulong pathHash)
-        {
-            if (!this._chunks.TryGetValue(pathHash, out WadChunk chunk))
-                ThrowHelper.ThrowArgumentException(nameof(pathHash), $"Failed to find chunk: {pathHash}");
-
-            return chunk;
-        }
-
-        private void Dispose(bool disposing)
-        {
-            if (this.IsDiposed)
-                return;
-
-            if (disposing)
-            {
-                this._stream.Dispose();
-            }
-
-            this.IsDiposed = true;
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
     }
 }
