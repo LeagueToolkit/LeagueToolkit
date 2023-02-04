1b7332beb1ad7fe08a6c0c6f9acc7c5512fc6c87
Crauzer <filip.quitko@gmail.com>
2023-02-04 10:37:25 +0100

    un-shit WadUtils

--- src/LeagueToolkit/Core/Wad/WadUtils.cs
+++ src/LeagueToolkit/Core/Wad/WadUtils.cs
@@ -4,35 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LeagueToolkit.Core.Wad
+namespace LeagueToolkit.Core.Wad;
+
+public static class WadUtils
 {
-    public static class WadUtils
+    // TODO: Refactor this to use Span API
+    public static WadChunkCompression GetExtensionCompression(string extension)
     {
-        // TODO: Refactor this to use Span API
-        public static WadChunkCompression GetExtensionCompression(string extension)
+        if (string.IsNullOrEmpty(extension))
+            return WadChunkCompression.None;
+
+        if (extension[0] == '.')
+            extension = extension.Remove(0, 1);
+
+        if (extension.Contains("glsl") || extension.Contains("dx9") || extension.Contains("dx11"))
+            return WadChunkCompression.None;
+
+        switch (extension)
         {
-            if (string.IsNullOrEmpty(extension))
+            case "wpk":
+            case "bnk":
+            case "troybin":
+            case "inibin":
+            case "gfx":
+            case "png":
+            case "preload":
                 return WadChunkCompression.None;
-
-            if (extension[0] == '.')
-                extension = extension.Remove(0, 1);
-
-            if (extension.Contains("glsl") || extension.Contains("dx9") || extension.Contains("dx11"))
-                return WadChunkCompression.None;
-
-            switch (extension)
-            {
-                case "wpk":
-                case "bnk":
-                case "troybin":
-                case "inibin":
-                case "gfx":
-                case "png":
-                case "preload":
-                    return WadChunkCompression.None;
-                default:
-                    return WadChunkCompression.Zstd;
-            }
+            default:
+                return WadChunkCompression.Zstd;
         }
     }
 }
