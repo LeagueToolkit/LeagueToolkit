9c2c2a69d769495c4a0e0e32de512ba168f63417
Crauzer <filip.quitko@gmail.com>
2023-02-05 13:04:58 +0100

    switch to zstd library that is not garbage (#232)

--- src/LeagueToolkit/IO/ReleaseManifestFile/ReleaseManifest.cs
+++ src/LeagueToolkit/IO/ReleaseManifestFile/ReleaseManifest.cs
@@ -1,107 +1,94 @@
-﻿using LeagueToolkit.Helpers.Exceptions;
-using FlatSharp;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using FlatSharp;
+using LeagueToolkit.Helpers.Exceptions;
 using System.Text;
-using ZstdNet;
 
-namespace LeagueToolkit.IO.ReleaseManifestFile
+namespace LeagueToolkit.IO.ReleaseManifestFile;
+
+public class ReleaseManifest
 {
-    public class ReleaseManifest
+    public ulong ID { get; private set; }
+    public IList<ReleaseManifestBundle> Bundles => this._body.Bundles;
+    public IList<ReleaseManifestLanguage> Languages => this._body.Languages;
+    public IList<ReleaseManifestFile> Files => this._body.Files;
+    public IList<ReleaseManifestDirectory> Directories => this._body.Directories;
+    public IList<ReleaseManifestEncryptionKey> EncryptionKeys => this._body.EncryptionKeys;
+    public IList<ReleaseManifestChunkingParameter> ChunkingParameters => this._body.ChunkingParameters;
+
+    private readonly ReleaseManifestBody _body;
+
+    public ReleaseManifest(string fileLocation) : this(File.OpenRead(fileLocation)) { }
+
+    public ReleaseManifest(Stream stream)
     {
-        public ulong ID { get; private set; }
-        public IList<ReleaseManifestBundle> Bundles => this._body.Bundles;
-        public IList<ReleaseManifestLanguage> Languages => this._body.Languages;
-        public IList<ReleaseManifestFile> Files => this._body.Files;
-        public IList<ReleaseManifestDirectory> Directories => this._body.Directories;
-        public IList<ReleaseManifestEncryptionKey> EncryptionKeys => this._body.EncryptionKeys;
-        public IList<ReleaseManifestChunkingParameter> ChunkingParameters => this._body.ChunkingParameters;
+        using BinaryReader br = new(stream);
 
-        private readonly ReleaseManifestBody _body;
+        string magic = Encoding.ASCII.GetString(br.ReadBytes(4));
+        if (magic != "RMAN")
+            throw new InvalidFileSignatureException();
 
-        public ReleaseManifest(string fileLocation) : this(File.OpenRead(fileLocation)) { }
+        byte major = br.ReadByte();
+        byte minor = br.ReadByte();
+        // NOTE: only check major because minor version are compatabile forwards-backwards
+        if (major != 2)
+            throw new UnsupportedFileVersionException();
 
-        public ReleaseManifest(Stream stream)
+        //Could possibly be Compression Type
+        byte unknown = br.ReadByte();
+        if (unknown != 0)
+            throw new Exception("Unknown: " + unknown);
+
+        byte signatureType = br.ReadByte();
+        uint contentOffset = br.ReadUInt32();
+        uint compressedContentSize = br.ReadUInt32();
+        this.ID = br.ReadUInt64();
+        uint uncompressedContentSize = br.ReadUInt32();
+
+        br.BaseStream.Seek(contentOffset, SeekOrigin.Begin);
+        byte[] compressedFile = br.ReadBytes((int)compressedContentSize);
+
+        if (signatureType != 0)
         {
-            using (BinaryReader br = new BinaryReader(stream))
-            {
-                string magic = Encoding.ASCII.GetString(br.ReadBytes(4));
-                if (magic != "RMAN")
-                {
-                    throw new InvalidFileSignatureException();
-                }
-
-                byte major = br.ReadByte();
-                byte minor = br.ReadByte();
-                // NOTE: only check major because minor version are compatabile forwards-backwards
-                if (major != 2)
-                {
-                    throw new UnsupportedFileVersionException();
-                }
-
-                //Could possibly be Compression Type
-                byte unknown = br.ReadByte();
-                if (unknown != 0)
-                {
-                    throw new Exception("Unknown: " + unknown);
-                }
-
-                byte signatureType = br.ReadByte();
-                uint contentOffset = br.ReadUInt32();
-                uint compressedContentSize = br.ReadUInt32();
-                this.ID = br.ReadUInt64();
-                uint uncompressedContentSize = br.ReadUInt32();
-
-                br.BaseStream.Seek(contentOffset, SeekOrigin.Begin);
-                byte[] compressedFile = br.ReadBytes((int)compressedContentSize);
-
-                if (signatureType != 0)
-                {
-                    byte[] signature = br.ReadBytes(256);
-                    // NOTE: verify signature here
-                }
-
-                using var decompressor = new Decompressor();
-                var uncompressedFile = decompressor.Unwrap(compressedFile).ToArray();
-
-                this._body = ReleaseManifestBody.Serializer.Parse(uncompressedFile);
-            }
+            byte[] signature = br.ReadBytes(256);
+            // NOTE: verify signature here
         }
 
-        public void Write(string fileLocation) => Write(File.Create(fileLocation));
+        using var decompressor = new ZstdSharp.Decompressor();
+        var uncompressedFile = decompressor.Unwrap(compressedFile).ToArray();
 
-        public void Write(Stream stream, bool leaveOpen = false)
-        {
-            byte[] magic = Encoding.ASCII.GetBytes("RMAN");
-            byte major = 2;
-            byte minor = 0;
-            byte unknown = 0;
-            byte signatureType = 0;
-            int contentOffset = 4 + 4 + 4 + 4 + 8 + 4;
+        this._body = ReleaseManifestBody.Serializer.Parse(uncompressedFile);
+    }
 
-            byte[] uncompressedFile = new byte[ReleaseManifestBody.Serializer.GetMaxSize(this._body)];
-            int uncompressedContentSize = ReleaseManifestBody.Serializer.Write(uncompressedFile, this._body);
-            Array.Resize(ref uncompressedFile, uncompressedContentSize);
+    public void Write(string fileLocation) => Write(File.Create(fileLocation));
 
-            using var compressor = new Compressor();
-            var compressedFile = compressor.Wrap(uncompressedFile).ToArray();
+    public void Write(Stream stream, bool leaveOpen = false)
+    {
+        byte[] magic = Encoding.ASCII.GetBytes("RMAN");
+        byte major = 2;
+        byte minor = 0;
+        byte unknown = 0;
+        byte signatureType = 0;
+        int contentOffset = 4 + 4 + 4 + 4 + 8 + 4;
 
-            int compressedContentSize = compressedFile.Length;
+        byte[] uncompressedFile = new byte[ReleaseManifestBody.Serializer.GetMaxSize(this._body)];
+        int uncompressedContentSize = ReleaseManifestBody.Serializer.Write(uncompressedFile, this._body);
+        Array.Resize(ref uncompressedFile, uncompressedContentSize);
 
-            using (BinaryWriter bw = new BinaryWriter(stream, Encoding.UTF8, leaveOpen))
-            {
-                bw.Write(magic);
-                bw.Write(major);
-                bw.Write(minor);
-                bw.Write(unknown);
-                bw.Write(signatureType);
-                bw.Write(contentOffset);
-                bw.Write(compressedContentSize);
-                bw.Write(this.ID);
-                bw.Write(uncompressedContentSize);
-                bw.Write(compressedFile);
-            }
-        }
+        using var compressor = new ZstdSharp.Compressor();
+        var compressedFile = compressor.Wrap(uncompressedFile).ToArray();
+
+        int compressedContentSize = compressedFile.Length;
+
+        using BinaryWriter bw = new(stream, Encoding.UTF8, leaveOpen);
+
+        bw.Write(magic);
+        bw.Write(major);
+        bw.Write(minor);
+        bw.Write(unknown);
+        bw.Write(signatureType);
+        bw.Write(contentOffset);
+        bw.Write(compressedContentSize);
+        bw.Write(this.ID);
+        bw.Write(uncompressedContentSize);
+        bw.Write(compressedFile);
     }
 }
