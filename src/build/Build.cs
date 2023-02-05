39692314c2b35e3b8b2f4a6de04d30450dd168c2
Crauzer <filip.quitko@gmail.com>
2023-02-05 16:34:40 +0100

    we're getting somewhere

--- src/build/Build.cs
+++ src/build/Build.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Linq;
+using Microsoft.Build.Construction;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
@@ -24,9 +27,67 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    Target Clean => _ => _.Before(Restore).Executes(() => { });
+    [Solution]
+    readonly Solution Solution;
 
-    Target Restore => _ => _.Executes(() => { });
+    [GitRepository]
+    readonly GitRepository GitRepository;
 
-    Target Compile => _ => _.DependsOn(Restore).Executes(() => { });
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    Target Clean =>
+        _ =>
+            _.Before(Restore)
+                .Executes(() =>
+                {
+                    SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+
+                    EnsureCleanDirectory(ArtifactsDirectory);
+                });
+
+    Target Restore =>
+        _ =>
+            _.Executes(() =>
+            {
+                DotNetTasks.DotNetRestore(s => s.SetProjectFile(Solution));
+            });
+
+    Target Compile =>
+        _ =>
+            _.DependsOn(Restore)
+                .Executes(() =>
+                {
+                    DotNetTasks.DotNetBuild(
+                        s => s.SetProjectFile(Solution).SetConfiguration(Configuration).EnableNoRestore()
+                    );
+                });
+
+    Target Test =>
+        _ =>
+            _.DependsOn(Compile)
+                .Executes(() =>
+                {
+                    DotNetTasks.DotNetTest(
+                        s =>
+                            s.SetProjectFile(Solution).SetConfiguration(Configuration).EnableNoRestore().EnableNoBuild()
+                    );
+                });
+
+    Target Pack =>
+        _ =>
+            _.DependsOn(Compile)
+                .Executes(() =>
+                {
+                    DotNetTasks.DotNetPack(
+                        s =>
+                            s.SetProject(Solution)
+                                .SetOutputDirectory(ArtifactsDirectory)
+                                .SetIncludeSymbols(true)
+                                .SetConfiguration(Configuration)
+                                .EnableNoRestore()
+                                .EnableNoBuild()
+                    );
+                });
 }
