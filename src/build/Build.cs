72321b8f1937bc7a17cb8727b8769e9fc694c44f
Crauzer <filip.quitko@gmail.com>
2023-02-05 15:59:32 +0100

    idk

--- src/build/Build.cs
+++ src/build/Build.cs
@@ -19,26 +19,14 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-        });
+    Target Clean => _ => _.Before(Restore).Executes(() => { });
 
-    Target Restore => _ => _
-        .Executes(() =>
-        {
-        });
+    Target Restore => _ => _.Executes(() => { });
 
-    Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-        });
-
+    Target Compile => _ => _.DependsOn(Restore).Executes(() => { });
 }
