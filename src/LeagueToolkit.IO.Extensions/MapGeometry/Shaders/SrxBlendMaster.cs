d2bcc7f7e3a3c2f0bd00d9f926121ad2db69bf22
Crauzer <filip.quitko@gmail.com>
2023-02-12 19:05:57 +0100

    SrxBlendMaster

---
+++ src/LeagueToolkit.IO.Extensions/MapGeometry/Shaders/SrxBlendMaster.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Diagnostics;
+using LeagueToolkit.Core.Environment;
+using LeagueToolkit.Core.Renderer;
+using LeagueToolkit.IO.MapGeometryFile;
+using LeagueToolkit.Meta.Classes;
+using SharpGLTF.Schema2;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using GltfImage = SharpGLTF.Schema2.Image;
+using TextureRegistry = System.Collections.Generic.Dictionary<string, SharpGLTF.Schema2.Image>;
+
+namespace LeagueToolkit.IO.Extensions.MapGeometry.Shaders;
+
+internal sealed class SrxBlendMaster : IMaterialAdapter
+{
+    private const string DEFAULT_DIFFUSE_TEXTURE = "ASSETS/Maps/KitPieces/SRX/textures/Earth_RockSpike_A.dds";
+
+    public void InitializeMaterial(
+        Material gltfMaterial,
+        StaticMaterialDef materialDef,
+        EnvironmentAssetMesh mesh,
+        TextureRegistry textureRegistry,
+        ModelRoot root,
+        MapGeometryGltfConversionContext context
+    )
+    {
+        gltfMaterial.WithUnlit();
+
+        InitializeMaterialBaseColorChannel(gltfMaterial, materialDef, mesh, root, textureRegistry, context);
+    }
+
+    private static void InitializeMaterialBaseColorChannel(
+        Material gltfMaterial,
+        StaticMaterialDef materialDef,
+        EnvironmentAssetMesh mesh,
+        ModelRoot root,
+        TextureRegistry textureRegistry,
+        MapGeometryGltfConversionContext context
+    )
+    {
+        // Resolve diffuse sampler definition, return if not found
+        StaticMaterialShaderSamplerDef samplerDef = materialDef.SamplerValues.FirstOrDefault(
+            x => x.Value.SamplerName is "DiffuseTexture"
+        );
+        samplerDef ??= new() { TextureName = DEFAULT_DIFFUSE_TEXTURE };
+
+        gltfMaterial.WithChannelTexture(
+            "BaseColor",
+            0,
+            TextureUtils.CreateGltfImage(samplerDef.TextureName, root, textureRegistry, context)
+        );
+    }
+}
