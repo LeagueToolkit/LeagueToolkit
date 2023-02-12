304d54199a0722b6bf0a8e2a408d87f6fea05491
Crauzer <filip.quitko@gmail.com>
2023-02-12 19:55:56 +0100

    SrxBlendEarthRocks

--- src/LeagueToolkit.IO.Extensions/MapGeometry/MapGeometryGltfExtensions.cs
+++ src/LeagueToolkit.IO.Extensions/MapGeometry/MapGeometryGltfExtensions.cs
@@ -61,6 +61,7 @@ namespace LeagueToolkit.IO.MapGeometryFile
                     new DefaultEnvFlatAlphaTestDoubleSided()
                 },
                 { Fnv1a.HashLower("Shaders/Environment/SRX_Blend_Earth_Island"), new SrxBlendEarthIsland() },
+                { Fnv1a.HashLower("Shaders/Environment/SRX_Blend_Earth_Rocks"), new SrxBlendEarthRocks() },
                 { Fnv1a.HashLower("Shaders/Environment/SRX_Blend_Generic_Island"), new SrxBlendGenericIsland() },
                 { Fnv1a.HashLower("Shaders/Environment/SRX_Blend_Infernal_Island"), new SrxBlendInfernalIsland() },
                 { Fnv1a.HashLower("Shaders/Environment/SRX_Blend_Master"), new SrxBlendMaster() },
