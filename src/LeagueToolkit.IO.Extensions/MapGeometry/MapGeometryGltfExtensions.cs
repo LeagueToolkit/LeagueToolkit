8b6c264312c0a1323aac0e4e8f86846ef5e7eda9
Crauzer <filip.quitko@gmail.com>
2023-02-12 20:36:14 +0100

    SrxBlendCloudGround

--- src/LeagueToolkit.IO.Extensions/MapGeometry/MapGeometryGltfExtensions.cs
+++ src/LeagueToolkit.IO.Extensions/MapGeometry/MapGeometryGltfExtensions.cs
@@ -61,6 +61,7 @@ namespace LeagueToolkit.IO.MapGeometryFile
                     new DefaultEnvFlatAlphaTestDoubleSided()
                 },
                 { Fnv1a.HashLower("Shaders/Environment/SRX_Blend_Chemtech_Decal"), new SrxBlendChemtechDecal() },
+                { Fnv1a.HashLower("Shaders/Environment/SRX_Blend_Cloud_Ground"), new SrxBlendCloudGround() },
                 { Fnv1a.HashLower("Shaders/Environment/SRX_Blend_Earth_Island"), new SrxBlendEarthIsland() },
                 { Fnv1a.HashLower("Shaders/Environment/SRX_Blend_Earth_Rocks"), new SrxBlendEarthRocks() },
                 { Fnv1a.HashLower("Shaders/Environment/SRX_Blend_Generic_Island"), new SrxBlendGenericIsland() },
