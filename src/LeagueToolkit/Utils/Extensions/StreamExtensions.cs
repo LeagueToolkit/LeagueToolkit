c44135f627513e3ccff2f888b054eb3712992b6d
Crauzer <18646077+Crauzer@users.noreply.github.com>
2023-04-02 11:18:34 +0200

    Update StreamExtensions.cs

--- src/LeagueToolkit/Utils/Extensions/StreamExtensions.cs
+++ src/LeagueToolkit/Utils/Extensions/StreamExtensions.cs
@@ -1,19 +1,18 @@
-﻿namespace LeagueToolkit.Utils.Extensions
-{
-    public static class StreamExtensions
-    {
-        public static void ReadExact(this Stream stream, Span<byte> buffer)
-        {
-            int totalBytesRead = 0;
-            int bytesRead;
-            do
-            {
-                bytesRead = stream.Read(buffer[totalBytesRead..]);
-                totalBytesRead += bytesRead;
-            } while (bytesRead is not 0);
+﻿namespace LeagueToolkit.Utils.Extensions;
 
-            if (totalBytesRead != buffer.Length)
-                throw new IOException($"Failed to read {buffer.Length} bytes, bytesRead: {totalBytesRead}");
-        }
+public static class StreamExtensions
+{
+    public static void ReadExact(this Stream stream, Span<byte> buffer)
+    {
+        int totalBytesRead = 0;
+        int bytesRead;
+        do
+        {
+            bytesRead = stream.Read(buffer[totalBytesRead..]);
+            totalBytesRead += bytesRead;
+        } while (bytesRead is not 0);
+
+        if (totalBytesRead != buffer.Length)
+            throw new IOException($"Failed to read {buffer.Length} bytes, bytesRead: {totalBytesRead}");
     }
 }
